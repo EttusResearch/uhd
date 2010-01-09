4e94574aeaa46c671bba0a6fc83cb967d228c880
Josh Blum <josh@joshknows.com>
2010-01-08 19:11:58 -0800

    Created subcomponent for usrp dboards. Only populated the interface though.

---
+++ usrp_dboard/lib/interface.cpp
@@ -0,0 +1,13 @@
+//
+// Copyright 2010 Ettus Research LLC
+//
+
+#include <usrp_dboard/interface.hpp>
+
+usrp_dboard::interface::interface(void){
+    /* NOP */
+}
+
+usrp_dboard::interface::~interface(void){
+    /* NOP */
+}
