d8052db8e5614935241a4824a3f9711bfe34507d
Philip Balister <philip@opensdr.com>
2010-04-02 19:49:45 +0000

    Convert to use register include file. Fix peek/poke16 program. Add program to cycle through LED's and test buttons. Only pushbuttons tested at the moment.

--- host/apps/omap_debug/usrp-e-ctl.c
+++ host/apps/omap_debug/usrp-e-ctl.c
@@ -2,44 +2,44 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <fcntl.h>
-#include <linux/ioctl.h>
+#include <sys/ioctl.h>
 
-#include "usrp1_e.h"
+#include "usrp_e.h"
 
-// Usage: usrp1_e_ctl w|r offset number_of_values val1 val2 ....
+// Usage: usrp_e_ctl w|r offset number_of_values val1 val2 ....
 
 int main(int argc, char *argv[])
 {
 	int fp, i, cnt, ret;
-	struct usrp1_e_ctl *ctl_data;
+	struct usrp_e_ctl16 *ctl_data;
 
 	if (argc < 4) {
-		printf("Usage: usrp1_e_ctl w|r offset number_of_values val1 val2 ....\n");
+		printf("Usage: usrp_e_ctl w|r offset number_of_values val1 val2 ....\n");
 		exit(-1);
 	}
 
 	cnt = atoi(argv[3]);
 
-	ctl_data = malloc(sizeof(struct usrp1_e_ctl) + cnt*2);
+	ctl_data = malloc(sizeof(struct usrp_e_ctl16) + cnt*2);
 
 	ctl_data->offset = atoi(argv[2]);
 	ctl_data->count  = cnt;
 
-	printf("Sizeof usrp1_e_ctl struct = %d\n", sizeof(struct usrp1_e_ctl));
+	printf("Sizeof usrp1_e_ctl struct = %d\n", sizeof(struct usrp_e_ctl16));
 
-	fp = open("/dev/usrp1_e0", O_RDWR);
+	fp = open("/dev/usrp_e0", O_RDWR);
 	printf("fp = %d\n", fp);
 
 	if (*argv[1] == 'w') {
 		for (i=0; i<cnt; i++)
 			ctl_data->buf[i] = atoi(argv[4+i]);
 
-		ret = ioctl(fp, USRP1_E_WRITE_CTL, ctl_data);
+		ret = ioctl(fp, USRP_E_WRITE_CTL16, ctl_data);
 		printf("Return value from write ioctl = %d\n", ret);
 	}
 
 	if (*argv[1] == 'r') {
-		ret = ioctl(fp, USRP1_E_READ_CTL, ctl_data);
+		ret = ioctl(fp, USRP_E_READ_CTL16, ctl_data);
 		printf("Return value from write ioctl = %d\n", ret);
 
 		for (i=0; i<ctl_data->count; i++) {
