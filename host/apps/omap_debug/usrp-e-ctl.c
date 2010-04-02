5d7a5e66c05290c5e3957457680313a2e427af08
Philip Balister <philip@opensdr.com>
2010-04-02 21:16:13 +0000

    Don't need to use malloc to get the correct sized struct anymore.

--- host/apps/omap_debug/usrp-e-ctl.c
+++ host/apps/omap_debug/usrp-e-ctl.c
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int fp, i, cnt, ret;
-	struct usrp_e_ctl16 *ctl_data;
+	struct usrp_e_ctl16 ctl_data;
 
 	if (argc < 4) {
 		printf("Usage: usrp_e_ctl w|r offset number_of_values val1 val2 ....\n");
@@ -20,32 +20,28 @@ int main(int argc, char *argv[])
 
 	cnt = atoi(argv[3]);
 
-	ctl_data = malloc(sizeof(struct usrp_e_ctl16) + cnt*2);
-
-	ctl_data->offset = atoi(argv[2]);
-	ctl_data->count  = cnt;
-
-	printf("Sizeof usrp1_e_ctl struct = %d\n", sizeof(struct usrp_e_ctl16));
+	ctl_data.offset = atoi(argv[2]);
+	ctl_data.count  = cnt;
 
 	fp = open("/dev/usrp_e0", O_RDWR);
 	printf("fp = %d\n", fp);
 
 	if (*argv[1] == 'w') {
 		for (i=0; i<cnt; i++)
-			ctl_data->buf[i] = atoi(argv[4+i]);
+			ctl_data.buf[i] = atoi(argv[4+i]);
 
-		ret = ioctl(fp, USRP_E_WRITE_CTL16, ctl_data);
+		ret = ioctl(fp, USRP_E_WRITE_CTL16, &ctl_data);
 		printf("Return value from write ioctl = %d\n", ret);
 	}
 
 	if (*argv[1] == 'r') {
-		ret = ioctl(fp, USRP_E_READ_CTL16, ctl_data);
+		ret = ioctl(fp, USRP_E_READ_CTL16, &ctl_data);
 		printf("Return value from write ioctl = %d\n", ret);
 
-		for (i=0; i<ctl_data->count; i++) {
+		for (i=0; i<ctl_data.count; i++) {
 			if (!(i%8))
 				printf("\nData at %4d :", i);
-			printf(" %5d", ctl_data->buf[i]);
+			printf(" %5d", ctl_data.buf[i]);
 		}
 		printf("\n");
 	}
