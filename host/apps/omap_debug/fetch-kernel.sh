e526627270cf2846d0cba0d30a5947621c545be1
Philip Balister <philip@opensdr.com>
2010-05-02 08:52:36 +0000

    Update IP address for my home desktop. Change module version to 2.6.33.

--- host/apps/omap_debug/fetch-kernel.sh
+++ host/apps/omap_debug/fetch-kernel.sh
@@ -1,7 +1,7 @@
 if [ $GHQ ]; then
 	scp $GHQ_USER@astro:/workspace/usrp1-e-dev/kernel_usrp/arch/arm/boot/uImage /media/mmcblk0p1/uImage
 else
-	scp balister@192.168.1.167:src/git/kernel_usrp/arch/arm/boot/uImage /media/mmcblk0p1/uImage
+	scp balister@192.168.1.10:src/git/kernel_usrp/arch/arm/boot/uImage /media/mmcblk0p1/uImage
 fi
 sync
 
