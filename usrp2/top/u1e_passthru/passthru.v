45d92a0610582672cea4f1d97d116af00eac7bef
Matt Ettus <matt@ettus.com>
2010-05-07 10:27:33 -0700

    SPI passthru for programming clock gen chip on brand new boards

---
+++ usrp2/top/u1e_passthru/passthru.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+
+module passthru
+  (input overo_gpio0, 
+   output cgen_sclk, 
+   output cgen_sen_b, 
+   output cgen_mosi, 
+   input fpga_cfg_din,
+   input fpga_cfg_cclk
+   );
+   
+   assign cgen_sclk = fpga_cfg_cclk;
+   assign cgen_sen_b = overo_gpio0;
+   assign cgen_mosi = fpga_cfg_din;
+   
+   
+endmodule // passthru
