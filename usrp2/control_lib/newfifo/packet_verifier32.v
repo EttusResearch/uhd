d3e6f630945076cc9d5fb91e86e1caf0b554ac89
Matt Ettus <matt@ettus.com>
2010-05-21 15:56:18 -0700

    fifo36_to_ll8 and fifo pacer need a real fifo between them or they deadlock (by design)

--- usrp2/control_lib/newfifo/packet_verifier32.v
+++ usrp2/control_lib/newfifo/packet_verifier32.v
@@ -7,10 +7,17 @@ module packet_verifier32
 
    wire [7:0] 	     ll_data;
    wire 	     ll_sof_n, ll_eof_n, ll_src_rdy_n, ll_dst_rdy;
+   wire [35:0] 	     data_int;
+   wire 	     src_rdy_int, dst_rdy_int;
+   
+   fifo_short #(.WIDTH(36)) fifo_short
+     (.clk(clk), .reset(reset), .clear(clear),
+      .datain(data_i), .src_rdy_i(src_rdy_i), .dst_rdy_o(dst_rdy_o),
+      .dataout(data_int), .src_rdy_o(src_rdy_int), .dst_rdy_i(dst_rdy_int));
    
    fifo36_to_ll8 f36_to_ll8
      (.clk(clk), .reset(reset), .clear(clear),
-      .f36_data(data_i), .f36_src_rdy_i(src_rdy_i), .f36_dst_rdy_o(dst_rdy_o),
+      .f36_data(data_int), .f36_src_rdy_i(src_rdy_int), .f36_dst_rdy_o(dst_rdy_int),
       .ll_data(ll_data), .ll_sof_n(ll_sof_n), .ll_eof_n(ll_eof_n),
       .ll_src_rdy_n(ll_src_rdy_n), .ll_dst_rdy_n(~ll_dst_rdy));
    
