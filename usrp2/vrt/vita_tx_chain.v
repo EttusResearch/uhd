0cf5dc0aac63940358f13db6f60ef44b55e78a50
Matt Ettus <matt@ettus.com>
2010-07-20 15:22:19 -0700

    introduce new error types

--- usrp2/vrt/vita_tx_chain.v
+++ usrp2/vrt/vita_tx_chain.v
@@ -12,30 +12,39 @@ module vita_tx_chain
     output [15:0] dac_a, output [15:0] dac_b,
     output underrun, output run,
     output [31:0] debug);
+
+   localparam MAXCHAN = 1;
+   localparam FIFOWIDTH = 5+64+(32*MAXCHAN);
+
+   wire [FIFOWIDTH-1:0] tx1_data;
+   wire 		tx1_src_rdy, tx1_dst_rdy;
+   wire 		clear_vita;
+   wire [31:0] 		sample_tx;
+   wire [31:0] 		streamid, message;
+   wire 		trigger, sent;
+   wire [31:0] 		debug_vtc, debug_vtd, debug_tx_dsp;
+
+   wire 		error;
+   wire [3:0] 		error_code;
    
-   wire [31:0] 	  debug_vtc, debug_vtd, debug_tx_dsp;
-   wire [99:0] 	  tx1_data;
-   wire 	  tx1_src_rdy, tx1_dst_rdy;
-   wire 	  clear_vita;
-   wire [31:0] 	  sample_tx;
-   wire [31:0] 	  streamid, message;
-   wire 	  trigger, sent;
-   
+   assign underrun = error;
+   assign message = {28'h0,error_code};
+      
    setting_reg #(.my_addr(BASE_CTRL+2), .at_reset(0)) sr_streamid
      (.clk(clk),.rst(reset),.strobe(set_stb),.addr(set_addr),
       .in(set_data),.out(streamid),.changed());
 
-   vita_tx_deframer #(.BASE(BASE_CTRL), .MAXCHAN(1)) vita_tx_deframer
+   vita_tx_deframer #(.BASE(BASE_CTRL), .MAXCHAN(MAXCHAN)) vita_tx_deframer
      (.clk(clk), .reset(reset), .clear(clear_vita),
       .set_stb(set_stb),.set_addr(set_addr),.set_data(set_data),
       .data_i(tx_data_i), .src_rdy_i(tx_src_rdy_i), .dst_rdy_o(tx_dst_rdy_o),
       .sample_fifo_o(tx1_data), .sample_fifo_src_rdy_o(tx1_src_rdy), .sample_fifo_dst_rdy_i(tx1_dst_rdy),
       .debug(debug_vtd) );
 
-   vita_tx_control #(.BASE(BASE_CTRL), .WIDTH(32)) vita_tx_control
+   vita_tx_control #(.BASE(BASE_CTRL), .WIDTH(32*MAXCHAN)) vita_tx_control
      (.clk(clk), .reset(reset), .clear(clear_vita),
       .set_stb(set_stb),.set_addr(set_addr),.set_data(set_data),
-      .vita_time(vita_time),.underrun(underrun),
+      .vita_time(vita_time),.error(error),.error_code(error_code),
       .sample_fifo_i(tx1_data), .sample_fifo_src_rdy_i(tx1_src_rdy), .sample_fifo_dst_rdy_o(tx1_dst_rdy),
       .sample(sample_tx), .run(run), .strobe(strobe_tx),
       .debug(debug_vtc) );
@@ -51,7 +60,7 @@ module vita_tx_chain
       if(REPORT_ERROR==1)
 	gen_context_pkt #(.PROT_ENG_FLAGS(PROT_ENG_FLAGS)) gen_tx_err_pkt
 	  (.clk(clk), .reset(reset), .clear(clear_vita),
-	   .trigger(underrun), .sent(), 
+	   .trigger(error), .sent(), 
 	   .streamid(streamid), .vita_time(vita_time), .message(message),
 	   .data_o(err_data_o), .src_rdy_o(err_src_rdy_o), .dst_rdy_i(err_dst_rdy_i));
    endgenerate
--- usrp2/vrt/vita_tx_control.v
+++ usrp2/vrt/vita_tx_control.v
@@ -6,10 +6,11 @@ module vita_tx_control
     input set_stb, input [7:0] set_addr, input [31:0] set_data,
     
     input [63:0] vita_time,
-    output underrun,
+    output reg [3:0] error_code,
+    output error,
 
     // From vita_tx_deframer
-    input [4+64+WIDTH-1:0] sample_fifo_i,
+    input [5+64+WIDTH-1:0] sample_fifo_i,
     input sample_fifo_src_rdy_i,
     output sample_fifo_dst_rdy_o,
     
@@ -20,14 +21,16 @@ module vita_tx_control
 
     output [31:0] debug
     );
-   
-   assign sample = sample_fifo_i[4+64+WIDTH-1:4+64];
+
+   assign sample = sample_fifo_i[5+64+WIDTH-1:5+64];
 
    wire [63:0] send_time = sample_fifo_i[63:0];
    wire        eop = sample_fifo_i[64];
    wire        eob = sample_fifo_i[65];
    wire        sob = sample_fifo_i[66];
    wire        send_at = sample_fifo_i[67];
+   wire        seqnum_err = sample_fifo_i[68];
+   
    wire        now, early, late, too_early;
 
    // FIXME ignore too_early for now for timing reasons
@@ -41,7 +44,13 @@ module vita_tx_control
    localparam IBS_RUN = 1;  // FIXME do we need this?
    localparam IBS_CONT_BURST = 2;
    localparam IBS_UNDERRUN = 3;
-   localparam IBS_UNDERRUN_DONE = 4;
+   localparam IBS_TIME_ERROR = 4;
+   localparam IBS_SEQ_ERROR = 5;
+   localparam IBS_ERROR_DONE = 7;
+
+   localparam CODE_UNDERRUN = 2;
+   localparam CODE_SEQ_ERROR = 4;
+   localparam CODE_TIME_ERROR = 8;
    
    reg [2:0] ibs_state;
 
@@ -57,10 +66,12 @@ module vita_tx_control
        case(ibs_state)
 	 IBS_IDLE :
 	   if(sample_fifo_src_rdy_i)
-	     if(~send_at | now)
+	     if(seqnum_err)
+	       ibs_state <= IBS_SEQ_ERROR;
+	     else if(~send_at | now)
 	       ibs_state <= IBS_RUN;
 	     else if(late | too_early)
-	       ibs_state <= IBS_UNDERRUN;
+	       ibs_state <= IBS_TIME_ERROR;
 	 
 	 IBS_RUN :
 	   if(strobe)
@@ -74,24 +85,40 @@ module vita_tx_control
 
 	 IBS_CONT_BURST :
 	   if(strobe)
-	     ibs_state <= IBS_UNDERRUN_DONE;
+	     ibs_state <= IBS_ERROR_DONE;
 	   else if(sample_fifo_src_rdy_i)
-	     ibs_state <= IBS_RUN;
+	     if(seqnum_err)
+	       ibs_state <= IBS_SEQ_ERROR;
+	     else
+	       ibs_state <= IBS_RUN;
 	 
 	 IBS_UNDERRUN :
-	   if(sample_fifo_src_rdy_i & eop)
-	     ibs_state <= IBS_UNDERRUN_DONE;
-
-	 IBS_UNDERRUN_DONE :
+	   begin
+	      error_code <= CODE_UNDERRUN;
+	      if(sample_fifo_src_rdy_i & eop)
+		ibs_state <= IBS_ERROR_DONE;
+	   end
+	 IBS_TIME_ERROR :
+	   begin
+	      error_code <= CODE_TIME_ERROR;
+	      ibs_state <= IBS_ERROR_DONE;
+	   end
+	 IBS_SEQ_ERROR :
+	   begin
+	      error_code <= CODE_SEQ_ERROR;
+	      ibs_state <= IBS_ERROR_DONE;
+	   end
+	 IBS_ERROR_DONE :
 	   ;
+
        endcase // case (ibs_state)
 
    assign sample_fifo_dst_rdy_o = (ibs_state == IBS_UNDERRUN) | (strobe & (ibs_state == IBS_RUN));  // FIXME also cleanout
    assign run = (ibs_state == IBS_RUN) | (ibs_state == IBS_CONT_BURST);
-   assign underrun = (ibs_state == IBS_UNDERRUN_DONE);
+   assign error = (ibs_state == IBS_ERROR_DONE);
 
    assign debug = { { now,early,late,too_early,eop,eob,sob,send_at },
-		    { sample_fifo_src_rdy_i, sample_fifo_dst_rdy_o, strobe, run, underrun, ibs_state[2:0] },
+		    { sample_fifo_src_rdy_i, sample_fifo_dst_rdy_o, strobe, run, error, ibs_state[2:0] },
 		    { 8'b0 },
 		    { 8'b0 } };
    
