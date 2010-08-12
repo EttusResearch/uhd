bbe1965c18bb97d4a4d7b3cde1c343d147c23278
Ian Buckley <ianb@server2.(none)>
2010-08-12 14:35:06 -0700

    Regenerated FIFO's for extfifo. There are problems with certain configurations it seems. It is important that the fifo_xlnx_512x36_2clk_18to36 is generated with the "almost_full" pin even though it is not used in the application. if this pin is omitted the FPGA image doesn't work correctly

--- usrp2/coregen/fifo_xlnx_512x36_2clk_18to36.v
+++ usrp2/coregen/fifo_xlnx_512x36_2clk_18to36.v
@@ -46,6 +46,7 @@ module fifo_xlnx_512x36_2clk_18to36(
 	rd_en,
 	dout,
 	full,
+	almost_full,
 	empty,
 	prog_full);
 
@@ -58,6 +59,7 @@ input wr_en;
 input rd_en;
 output [35 : 0] dout;
 output full;
+output almost_full;
 output empty;
 output prog_full;
 
@@ -77,7 +79,7 @@ output prog_full;
 		.C_FAMILY("spartan3"),
 		.C_FULL_FLAGS_RST_VAL(0),
 		.C_HAS_ALMOST_EMPTY(0),
-		.C_HAS_ALMOST_FULL(0),
+		.C_HAS_ALMOST_FULL(1),
 		.C_HAS_BACKUP(0),
 		.C_HAS_DATA_COUNT(0),
 		.C_HAS_INT_CLK(0),
@@ -134,6 +136,7 @@ output prog_full;
 		.RD_EN(rd_en),
 		.DOUT(dout),
 		.FULL(full),
+		.ALMOST_FULL(almost_full),
 		.EMPTY(empty),
 		.PROG_FULL(prog_full),
 		.BACKUP(),
@@ -151,7 +154,6 @@ output prog_full;
 		.INT_CLK(),
 		.INJECTDBITERR(),
 		.INJECTSBITERR(),
-		.ALMOST_FULL(),
 		.WR_ACK(),
 		.OVERFLOW(),
 		.ALMOST_EMPTY(),
