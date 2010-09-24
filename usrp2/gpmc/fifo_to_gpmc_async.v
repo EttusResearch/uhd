1f77494788fa4fa8450aaf170055553bd0e5fe8e
Matt Ettus <matt@ettus.com>
2010-09-24 14:37:15 -0700

    allow for CS to rise before, at the same time, or after OE

--- usrp2/gpmc/fifo_to_gpmc_async.v
+++ usrp2/gpmc/fifo_to_gpmc_async.v
@@ -11,23 +11,22 @@ module fifo_to_gpmc_async
    input [15:0] frame_len);
 
    // Synchronize the async control signals
-   reg [1:0] 	cs_del, oe_del;
+   reg [2:0] 	cs_del, oe_del;
    reg [15:0] 	counter;
    
    always @(posedge clk)
      if(reset)
        begin
-	  cs_del <= 2'b11;
-	  oe_del <= 2'b11;
+	  cs_del <= 3'b11;
+	  oe_del <= 3'b11;
        end
      else
        begin
-	  cs_del <= { cs_del[0], EM_NCS };
-	  oe_del <= { oe_del[0], EM_NOE };
+	  cs_del <= { cs_del[1:0], EM_NCS };
+	  oe_del <= { oe_del[1:0], EM_NOE };
        end
 
-   //wire do_read = (~cs_del[0] & (oe_del == 2'b10));
-   wire do_read = (~cs_del[1] & (oe_del == 2'b01));  // change output on trailing edge
+   wire do_read = ( (~cs_del[1] | ~cs_del[2]) & (oe_del[1:0] == 2'b01));  // change output on trailing edge
    wire first_read = (counter == 0);
    wire last_read = ((counter+1) == frame_len);
 
