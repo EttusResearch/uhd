206a70cb823da9c338dcb83855a22ad251bb9cb7
Philip Balister <philip@opensdr.com>
2011-02-08 08:40:47 -0800

    Fix typo in usage message.

--- host/apps/omap_debug/usrp-e-crc-rw.c
+++ host/apps/omap_debug/usrp-e-crc-rw.c
@@ -231,7 +231,7 @@ int main(int argc, char *argv[])
 	};
 
 	if (argc < 4) {
-		printf("%s t|w|rw decimation data_size\n", argv[0]);
+		printf("%s r|w|rw decimation data_size\n", argv[0]);
 		return -1;
 	}
 
