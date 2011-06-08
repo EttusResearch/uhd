23c373f4ebea614a59f2032293b0264d93468fef
Matt Ettus <matt@ettus.com>
2011-06-07 19:46:49 -0700

    fix copyright notice

--- usrp2/top/tcl/ise_helper.tcl
+++ usrp2/top/tcl/ise_helper.tcl
@@ -1,8 +1,6 @@
 #
 # Copyright 2008 Ettus Research LLC
 # 
-# This file is part of GNU Radio
-# 
 # GNU Radio is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
