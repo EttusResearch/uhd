ed3439eebb51ab5ef8f184624212ded30a8544bb
Josh Blum <josh@joshknows.com>
2011-06-14 23:58:06 -0700

    b100: fix fx2_ctrl include to use relative path

--- host/lib/usrp/usrp_b100/b100_iface.hpp
+++ host/lib/usrp/usrp_b100/b100_iface.hpp
@@ -23,7 +23,7 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/utility.hpp>
 #include <uhd/transport/usb_zero_copy.hpp>
-#include "fx2_ctrl.hpp"
+#include "../fx2/fx2_ctrl.hpp"
 #include "b100_ctrl.hpp"
 
 /*!
