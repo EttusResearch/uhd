21db20f3b2c7c9464927134faa5587bf20017368
Josh Blum <josh@joshknows.com>
2011-06-15 12:23:58 -0700

    uhd: created benchmark rate app

---
+++ host/examples/benchmark_rate.cpp
@@ -0,0 +1,222 @@
+//
+// Copyright 2011 Ettus Research LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include <uhd/utils/thread_priority.hpp>
+#include <uhd/utils/safe_main.hpp>
+#include <uhd/usrp/multi_usrp.hpp>
+#include <boost/program_options.hpp>
+#include <boost/format.hpp>
+#include <boost/thread/thread.hpp>
+#include <iostream>
+#include <complex>
+
+namespace po = boost::program_options;
+
+unsigned long long num_overflows = 0;
+unsigned long long num_underflows = 0;
+unsigned long long num_rx_samps = 0;
+unsigned long long num_tx_samps = 0;
+
+/***********************************************************************
+ * Benchmark RX Rate
+ **********************************************************************/
+void benchmark_rx_rate(uhd::usrp::multi_usrp::sptr usrp){
+    uhd::set_thread_priority_safe();
+
+    //print pre-test summary
+    std::cout << boost::format(
+        "Testing receive rate %f Msps"
+    ) % (usrp->get_rx_rate()/1e6) << std::endl;
+
+    //setup variables and allocate buffer
+    uhd::rx_metadata_t md;
+    const size_t max_samps_per_packet = usrp->get_device()->get_max_recv_samps_per_packet();
+    std::vector<std::complex<float> > buff(max_samps_per_packet);
+
+    usrp->issue_stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
+    while (not boost::this_thread::interruption_requested()){
+        num_rx_samps += usrp->get_device()->recv(
+            &buff.front(), buff.size(), md,
+            uhd::io_type_t::COMPLEX_FLOAT32,
+            uhd::device::RECV_MODE_ONE_PACKET
+        );
+
+        //handle the error codes
+        switch(md.error_code){
+        case uhd::rx_metadata_t::ERROR_CODE_NONE:
+            break;
+
+        case uhd::rx_metadata_t::ERROR_CODE_OVERFLOW:
+            num_overflows++;
+            break;
+
+        default:
+            std::cerr << "Error code: " << md.error_code << std::endl;
+            std::cerr << "Unexpected error on recv, exit test..." << std::endl;
+            goto loop_done;
+        }
+
+    } loop_done:
+    usrp->issue_stream_cmd(uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS);
+}
+
+/***********************************************************************
+ * Benchmark TX Rate
+ **********************************************************************/
+void benchmark_tx_rate(uhd::usrp::multi_usrp::sptr usrp){
+    uhd::set_thread_priority_safe();
+
+    //print pre-test summary
+    std::cout << boost::format(
+        "Testing transmit rate %f Msps"
+    ) % (usrp->get_tx_rate()/1e6) << std::endl;
+
+    //setup variables and allocate buffer
+    uhd::tx_metadata_t md;
+    md.has_time_spec = false;
+    const size_t max_samps_per_packet = usrp->get_device()->get_max_send_samps_per_packet();
+    std::vector<std::complex<float> > buff(max_samps_per_packet);
+
+    while (not boost::this_thread::interruption_requested()){
+        num_tx_samps += usrp->get_device()->send(
+            &buff.front(), buff.size(), md,
+            uhd::io_type_t::COMPLEX_FLOAT32,
+            uhd::device::SEND_MODE_ONE_PACKET
+        );
+    }
+
+    //send a mini EOB packet
+    md.end_of_burst   = true;
+    usrp->get_device()->send("", 0, md,
+        uhd::io_type_t::COMPLEX_FLOAT32,
+        uhd::device::SEND_MODE_FULL_BUFF
+    );
+}
+
+void benchmark_tx_rate_async_helper(uhd::usrp::multi_usrp::sptr usrp){
+    //setup variables and allocate buffer
+    uhd::async_metadata_t async_md;
+
+    while (not boost::this_thread::interruption_requested()){
+
+        if (not usrp->get_device()->recv_async_msg(async_md)) continue;
+
+        //handle the error codes
+        switch(async_md.event_code){
+        case uhd::async_metadata_t::EVENT_CODE_BURST_ACK:
+            return;
+
+        case uhd::async_metadata_t::EVENT_CODE_UNDERFLOW:
+        case uhd::async_metadata_t::EVENT_CODE_UNDERFLOW_IN_PACKET:
+            num_underflows++;
+            break;
+
+        default:
+            std::cerr << "Event code: " << async_md.event_code << std::endl;
+            std::cerr << "Unexpected event on async recv, exit test..." << std::endl;
+            return;
+        }
+    }
+}
+
+/***********************************************************************
+ * Main code + dispatcher
+ **********************************************************************/
+int UHD_SAFE_MAIN(int argc, char *argv[]){
+
+    //variables to be set by po
+    std::string args;
+    double duration;
+    double rx_rate, tx_rate;
+
+    //setup the program options
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "help message")
+        ("args", po::value<std::string>(&args)->default_value(""), "single uhd device address args")
+        ("duration", po::value<double>(&duration)->default_value(10.0), "duration for the test in seconds")
+        ("rx_rate", po::value<double>(&rx_rate), "specify to perform a RX rate test (sps)")
+        ("tx_rate", po::value<double>(&tx_rate), "specify to perform a TX rate test (sps)")
+    ;
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    //print the help message
+    if (vm.count("help")){
+        std::cout << boost::format("UHD Benchmark Rate %s") % desc << std::endl;
+        std::cout <<
+        "    Specify --rx_rate for a receive-only test.\n"
+        "    Specify --tx_rate for a transmit-only test.\n"
+        "    Specify both options for a full-duplex test.\n"
+        << std::endl;
+        return ~0;
+    }
+
+    //create a usrp device
+    std::cout << std::endl;
+    uhd::device_addrs_t device_addrs = uhd::device::find(args);
+    if (device_addrs.empty()){
+        std::cerr << "Could not find any devices for: " << args << std::endl;
+        return ~0;
+    }
+    if (device_addrs.at(0).get("type", "") == "usrp1"){
+        std::cerr << "*** Warning! ***" << std::endl;
+        std::cerr << "Benchmark results will be inaccurate on USRP1 due to insufficient features.\n" << std::endl;
+    }
+    std::cout << boost::format("Creating the usrp device with: %s...") % args << std::endl;
+    uhd::usrp::multi_usrp::sptr usrp = uhd::usrp::multi_usrp::make(device_addrs.at(0));
+    std::cout << boost::format("Using Device: %s") % usrp->get_pp_string() << std::endl;
+
+    boost::thread_group thread_group;
+
+    //spawn the receive test thread
+    if (vm.count("rx_rate")){
+        usrp->set_rx_rate(rx_rate);
+        thread_group.create_thread(boost::bind(&benchmark_rx_rate, usrp));
+    }
+
+    //spawn the transmit test thread
+    if (vm.count("tx_rate")){
+        usrp->set_tx_rate(tx_rate);
+        thread_group.create_thread(boost::bind(&benchmark_tx_rate, usrp));
+        thread_group.create_thread(boost::bind(&benchmark_tx_rate_async_helper, usrp));
+    }
+
+    //sleep for the required duration
+    const long secs = long(duration);
+    const long usecs = (duration - secs)*1e6;
+    boost::this_thread::sleep(boost::posix_time::seconds(secs) + boost::posix_time::microseconds(usecs));
+
+    //interrupt and join the threads
+    thread_group.interrupt_all();
+    thread_group.join_all();
+
+    //print summary
+    std::cout << std::endl << boost::format(
+        "Benchmark rate summary:\n"
+        "  Num received samples: %u\n"
+        "  Num overflows detected: %u\n"
+        "  Num transmitted samples: %u\n"
+        "  Num underflows detected: %u\n"
+    ) % num_rx_samps % num_overflows % num_tx_samps % num_overflows << std::endl;
+
+    //finished
+    std::cout << std::endl << "Done!" << std::endl << std::endl;
+
+    return 0;
+}
