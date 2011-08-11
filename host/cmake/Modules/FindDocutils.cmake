f8d7ff36eede9ed175fd0efa8de70afdaa049094
Josh Blum <josh@joshknows.com>
2011-08-11 14:27:13 -0700

    uhd: also find RST2HTML_EXECUTABLE as rst2html.py

--- host/cmake/Modules/FindDocutils.cmake
+++ host/cmake/Modules/FindDocutils.cmake
@@ -16,6 +16,6 @@
 #
 
 ########################################################################
-FIND_PROGRAM(RST2HTML_EXECUTABLE rst2html)
+FIND_PROGRAM(RST2HTML_EXECUTABLE rst2html rst2html.py)
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Docutils DEFAULT_MSG RST2HTML_EXECUTABLE)
