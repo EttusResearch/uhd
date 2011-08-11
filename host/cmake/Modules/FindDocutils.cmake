064fcc6575c3c8d77a57ab9079885e64bee870f0
Nick Foster <nick@ettus.com>
2011-08-11 15:42:14 -0700

    save before you commit, thanks

--- host/cmake/Modules/FindDocutils.cmake
+++ host/cmake/Modules/FindDocutils.cmake
@@ -16,6 +16,6 @@
 #
 
 ########################################################################
-FIND_PROGRAM(RST2HTML_EXECUTABLE NAMED rst2html rst2html.py)
+FIND_PROGRAM(RST2HTML_EXECUTABLE NAMES rst2html rst2html.py)
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Docutils DEFAULT_MSG RST2HTML_EXECUTABLE)
