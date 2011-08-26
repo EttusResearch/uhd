d8a044097cb68f4bb9ed2727799568d534f631de
Josh Blum <josh@joshknows.com>
2011-08-26 13:27:19 -0700

    fpga: minor tweaks to build system

--- usrp2/top/python/check_timing.py
+++ usrp2/top/python/check_timing.py
@@ -21,10 +21,13 @@ import re
 def print_timing_constraint_summary(twr_file):
     output = ""
     keep = False
+    done = False
     for line in open(twr_file).readlines():
         if 'Derived Constraint Report' in line: keep = True
+        if 'constraint' in line and 'met' in line: done = True
+        if not keep and done: keep = True
         if keep: output += line
-        if 'constraint' in line and 'met' in line: break
+        if done: break
     print("\n\n"+output)
 
 if __name__=='__main__': map(print_timing_constraint_summary, sys.argv[1:])
