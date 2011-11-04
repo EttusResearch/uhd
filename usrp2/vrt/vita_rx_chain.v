ca02bf0358677043fb5d594a0f12413ba242c892
Matt Ettus <matt@ettus.com>
2011-11-04 14:45:32 -0700

    dsp: remove dsp_buffer and replace with simpler add_routing_header,   other funcs of dsp_buffer are done by double_buffer and dsp_engine

--- usrp2/vrt/vita_rx_chain.v
+++ usrp2/vrt/vita_rx_chain.v
@@ -72,9 +72,8 @@ module vita_rx_chain
       .access_skip_read(access_skip_read), .access_adr(access_adr), .access_len(access_len), 
       .access_dat_i(buf_to_dsp), .access_dat_o(dsp_to_buf));
    
-   dsp_framer36 #(.BUF_SIZE(FIFOSIZE), 
-		  .PORT_SEL(UNIT), 
-		  .PROT_ENG_FLAGS(PROT_ENG_FLAGS)) dsp0_framer36
+   add_routing_header #(.PORT_SEL(UNIT), 
+			.PROT_ENG_FLAGS(PROT_ENG_FLAGS)) dsp_routing_header
      (.clk(clk), .reset(reset), .clear(clear),
       .data_i(rx_data_int2), .src_rdy_i(rx_src_rdy_int2), .dst_rdy_o(rx_dst_rdy_int2),
       .data_o(rx_data_o), .src_rdy_o(rx_src_rdy_o), .dst_rdy_i(rx_dst_rdy_i) );
