cd34627d31b2c593151c0c8caeae20d6ec7a5121
Josh Blum <josh@joshknows.com>
2011-12-30 23:38:11 -0800

    uhd: squashed changes to if_addrs

--- host/lib/transport/if_addrs.cpp
+++ host/lib/transport/if_addrs.cpp
@@ -48,6 +48,17 @@ std::vector<uhd::transport::if_addrs_t> uhd::transport::get_if_addrs(void){
             if_addr.inet = sockaddr_to_ip_addr(iter->ifa_addr).to_string();
             if_addr.mask = sockaddr_to_ip_addr(iter->ifa_netmask).to_string();
             if_addr.bcast = sockaddr_to_ip_addr(iter->ifa_broadaddr).to_string();
+
+            //correct the bcast address when its same as the gateway
+            if (if_addr.inet == if_addr.bcast or sockaddr_to_ip_addr(iter->ifa_broadaddr) == boost::asio::ip::address_v4(0)){
+                //manually calculate broadcast address
+                //https://svn.boost.org/trac/boost/ticket/5198
+                const boost::uint32_t addr = sockaddr_to_ip_addr(iter->ifa_addr).to_ulong();
+                const boost::uint32_t mask = sockaddr_to_ip_addr(iter->ifa_netmask).to_ulong();
+                const boost::uint32_t bcast = (addr & mask) | ~mask;
+                if_addr.bcast = boost::asio::ip::address_v4(bcast).to_string();
+            }
+
             if_addrs.push_back(if_addr);
         }
         freeifaddrs(ifap);
