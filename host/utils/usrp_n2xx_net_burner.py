34627f1c7780d8c8578964c6594fef7a6640e727
Josh Blum <josh@joshknows.com>
2012-04-12 09:46:38 -0700

    utils: card/net burner string decode

--- host/utils/usrp_n2xx_net_burner.py
+++ host/utils/usrp_n2xx_net_burner.py
@@ -134,7 +134,7 @@ def command(*args):
         stderr=subprocess.STDOUT,
     )
     ret = p.wait()
-    verbose = p.stdout.read().decode()
+    verbose = p.stdout.read().decode('utf-8', errors='ignore')
     if ret != 0: raise Exception(verbose)
     return verbose
 
