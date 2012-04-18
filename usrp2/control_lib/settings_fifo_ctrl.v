1ddcad462f1d867ab432625b110446278c928874
Josh Blum <josh@joshknows.com>
2012-04-17 18:43:25 -0700

    fifo ctrl: Nseries timing meets with a single shortfifo

--- usrp2/control_lib/settings_fifo_ctrl.v
+++ usrp2/control_lib/settings_fifo_ctrl.v
@@ -19,7 +19,6 @@
 
 module settings_fifo_ctrl
     #(
-        parameter FIFO_DEPTH = 6, //64 entries depth
         parameter PROT_DEST = 0, //protocol framer destination
         parameter PROT_HDR = 1, //needs a protocol header?
         parameter ACK_SID = 0 //stream ID for packet ACK
@@ -79,7 +78,7 @@ module settings_fifo_ctrl
     wire command_fifo_full, command_fifo_empty;
     wire command_fifo_read, command_fifo_write;
 
-    medfifo #(.WIDTH(129), .DEPTH(FIFO_DEPTH-4)) command_fifo (
+    shortfifo #(.WIDTH(129)) command_fifo (
         .clk(clock), .rst(reset), .clear(clear),
         .datain({in_command_ticks, in_command_hdr, in_command_data, in_command_has_time}),
         .dataout({out_command_ticks, out_command_hdr, out_command_data, out_command_has_time}),
@@ -96,7 +95,7 @@ module settings_fifo_ctrl
     wire result_fifo_full, result_fifo_empty;
     wire result_fifo_read, result_fifo_write;
 
-    medfifo #(.WIDTH(64), .DEPTH(FIFO_DEPTH-4)) result_fifo (
+    shortfifo #(.WIDTH(64)) result_fifo (
         .clk(clock), .rst(reset), .clear(clear),
         .datain({in_result_hdr, in_result_data}),
         .dataout({out_result_hdr, out_result_data}),
