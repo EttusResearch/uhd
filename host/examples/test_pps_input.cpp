504c2e1f7ee237c73b13ab28ea928814c4efdc45
Josh Blum <josh@joshknows.com>
2013-08-13 11:39:36 -0700

    uhd: added time source option to PPS test

--- host/examples/test_pps_input.cpp
+++ host/examples/test_pps_input.cpp
@@ -31,12 +31,14 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
 
     //variables to be set by po
     std::string args;
+    std::string time_source;
 
     //setup the program options
     po::options_description desc("Allowed options");
     desc.add_options()
         ("help", "help message")
         ("args", po::value<std::string>(&args)->default_value(""), "single uhd device address args")
+        ("source", po::value<std::string>(&time_source)->default_value(""), "the time source (gpsdo, external) or blank for default")
     ;
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -53,6 +55,12 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
     std::cout << boost::format("Creating the usrp device with: %s...") % args << std::endl;
     uhd::usrp::multi_usrp::sptr usrp = uhd::usrp::multi_usrp::make(args);
     std::cout << boost::format("Using Device: %s") % usrp->get_pp_string() << std::endl;
+
+    //sleep off if gpsdo detected and time next pps already set
+    boost::this_thread::sleep(boost::posix_time::seconds(1));
+
+    //set time source if specified
+    if (not time_source.empty()) usrp->set_time_source(time_source);
 
     //set the time at an unknown pps (will throw if no pps)
     std::cout << std::endl << "Attempt to detect the PPS and set the time..." << std::endl << std::endl;
