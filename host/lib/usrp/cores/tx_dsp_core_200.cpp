44386f32b17ee74988178f9eb6d0b787b7863232
Michael West <michael.west@ettus.com>
2013-10-28 12:44:50 -0700

    CID 1104308:  Fixed uninitialized scalar.

--- host/lib/usrp/cores/tx_dsp_core_200.cpp
+++ host/lib/usrp/cores/tx_dsp_core_200.cpp
@@ -60,6 +60,9 @@ public:
     ):
         _iface(iface), _dsp_base(dsp_base), _ctrl_base(ctrl_base), _sid(sid)
     {
+        // previously uninitialized - assuming zero for all
+        _tick_rate = _link_rate = _host_extra_scaling = _fxpt_scalar_correction = 0.0;
+
         //init to something so update method has reasonable defaults
         _scaling_adjustment = 1.0;
         _dsp_extra_scaling = 1.0;
