9ea8bedb6bc3a1efd03721b56785c4bead7ab81a
Martin Braun <martin.braun@ettus.com>
2014-03-28 14:25:09 +0100

    uhd: Cal tools had ambiguous help message, referring to XCVR boards.

--- host/utils/uhd_cal_rx_iq_balance.cpp
+++ host/utils/uhd_cal_rx_iq_balance.cpp
@@ -121,7 +121,8 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
     if (vm.count("help")){
         std::cout << boost::format("USRP Generate RX IQ Balance Calibration Table %s") % desc << std::endl;
         std::cout <<
-            "This application measures leakage between RX and TX on an XCVR daughterboard to self-calibrate.\n"
+            "This application measures leakage between RX and TX on a transceiver daughterboard to self-calibrate.\n"
+            "Note: Not all daughterboards support this feature. Refer to the UHD manual for details.\n"
             << std::endl;
         return EXIT_FAILURE;
     }
