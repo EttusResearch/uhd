623f5295d8297794d85894ed174221b6d18e75c0
Ashish Chaudhari <ashish@ettus.com>
2014-04-28 13:08:39 -0700

    x300: Changed bus_clk frequency to 166.67MHz.

--- host/lib/usrp/x300/x300_impl.hpp
+++ host/lib/usrp/x300/x300_impl.hpp
@@ -53,7 +53,7 @@
 static const std::string X300_FW_FILE_NAME  = "usrp_x300_fw.bin";
 
 static const double X300_DEFAULT_TICK_RATE      = 200e6;        //Hz
-static const double X300_BUS_CLOCK_RATE         = 175e6;        //Hz
+static const double X300_BUS_CLOCK_RATE         = 166.666667e6; //Hz
 
 static const size_t X300_TX_HW_BUFF_SIZE        = 0x90000;      //576KiB
 static const size_t X300_TX_FC_RESPONSE_FREQ    = 8;            //per flow-control window
