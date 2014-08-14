e3826b1a671f4fe9e197b72a9c8dc0aa6976ea0c
Nicholas Corgan <nick.corgan@ettus.com>
2014-08-14 06:45:53 -0700

    Restoring compatibility with CMake 2.6
    * The UNSET command didn't exist back then, but using SET with no value does the same thing
    * CMake 2.6 doesn't like nested parantheses in IF statements, so tweak IF/ELSE statements

--- host/cmake/Modules/FindUSB1.cmake
+++ host/cmake/Modules/FindUSB1.cmake
@@ -23,7 +23,7 @@ FIND_PATH(LIBUSB_INCLUDE_DIRS
 set(libusb1_library_names usb-1.0 libusb-1.0)
 
 #libusb-1.0 compatible library on freebsd
-if((CMAKE_SYSTEM_NAME STREQUAL "FreeBSD") OR (CMAKE_SYSTEM_NAME STREQUAL "kFreeBSD"))
+if(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD" OR CMAKE_SYSTEM_NAME STREQUAL "kFreeBSD")
     list(APPEND libusb1_library_names usb)
 endif()
 
