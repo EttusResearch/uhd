6d99396a8355bb6273564857716f4504053d0882
Martin Braun <martin.braun@ettus.com>
2014-08-25 22:05:07 +0200

    uhd: Added a stop stream cmd to the rx example

--- host/examples/rx_samples_to_file.cpp
+++ host/examples/rx_samples_to_file.cpp
@@ -140,6 +140,9 @@ template<typename samp_type> void recv_to_file(
 				break;
 		}
     }
+
+    stream_cmd.stream_mode = uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS;
+    rx_stream->issue_stream_cmd(stream_cmd);
     
     if (outfile.is_open())
 		outfile.close();
