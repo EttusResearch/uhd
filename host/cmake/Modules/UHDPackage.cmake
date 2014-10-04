bc6edca2429564efc9fd685dc3b42639c01ff5a7
jtrutna <github@trutna.com>
2014-10-03 18:30:58 -0700

    Update UHDPackage.cmake
    Let's make the apt description a little more descriptive.

--- host/cmake/Modules/UHDPackage.cmake
+++ host/cmake/Modules/UHDPackage.cmake
@@ -111,9 +111,9 @@ ENDIF()
 ########################################################################
 # Setup CPack General
 ########################################################################
-SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Ettus Research - USRP Hardware Driver")
-SET(CPACK_PACKAGE_VENDOR              "Ettus Research LLC")
-SET(CPACK_PACKAGE_CONTACT             "Ettus Research <support@ettus.com>")
+SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Spire Fork of Ettus Research's USRP Hardware Driver")
+SET(CPACK_PACKAGE_VENDOR              "Spire")
+SET(CPACK_PACKAGE_CONTACT             "Spire <jint@spire.com>")
 SET(CPACK_PACKAGE_VERSION "${UHD_VERSION}")
 SET(CPACK_RESOURCE_FILE_WELCOME ${CMAKE_SOURCE_DIR}/README.md)
 SET(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_SOURCE_DIR}/LICENSE)
