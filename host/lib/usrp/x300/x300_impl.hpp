d655137c93c7622f86c0794efcf0044b009b1fb4
Ashish Chaudhari <ashish@ettus.com>
2014-10-08 09:52:12 -0700

    x300: Reduced the TX buffer from 576K to 520K
    - Trading performance for stability. This helps meet timing at the cost of a shorter processing time window between sends.
    - Bumped FPGA compat number to 8

--- host/lib/usrp/x300/x300_impl.hpp
+++ host/lib/usrp/x300/x300_impl.hpp
@@ -55,7 +55,7 @@ static const std::string X300_FW_FILE_NAME  = "usrp_x300_fw.bin";
 static const double X300_DEFAULT_TICK_RATE      = 200e6;        //Hz
 static const double X300_BUS_CLOCK_RATE         = 166.666667e6; //Hz
 
-static const size_t X300_TX_HW_BUFF_SIZE        = 0x90000;      //576KiB
+static const size_t X300_TX_HW_BUFF_SIZE        = 520*1024;      //512K SRAM buffer + 8K 2Clk FIFO
 static const size_t X300_TX_FC_RESPONSE_FREQ    = 8;            //per flow-control window
 
 static const size_t X300_RX_SW_BUFF_SIZE_ETH        = 0x2000000;//32MiB    For an ~8k frame size any size >32MiB is just wasted buffer space
