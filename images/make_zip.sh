b9ed377fcb9bd411cf7537b562fa7ac2314fb1d5
Martin Braun <martin.braun@ettus.com>
2014-10-13 11:39:28 +0200

    tools: Auto-build tgz image packages, too (not just zip)
    Also, made image creation process easier.

--- images/make_zip.sh
+++ images/make_zip.sh
@@ -1,17 +1,39 @@
 #!/bin/sh
 # Automatically run the make-zip-file process
+# Check we're in the right directory and all is set:
 if [ ! -e 'make_zip.sh' ]; then
-    echo 'Are you running this from the images/ directory?'
+    echo '[ERROR] Are you running this from the images/ directory?'
     exit 1
 fi
+if [ ! -e 'images' ]; then
+    echo 'images subdirectory does not exist. Please create it and put all the images you want to package in there.'
+    exit 1
+fi
+
+# Remove cruft before proceeding:
 if [ -e 'build' ]; then
     echo 'Please remove build subdirectory before proceeding.'
     exit 1
 fi
+if [ -e "images/LICENSE" ]; then
+	rm images/LICENSE
+fi
+TAGFILES=`ls images/*.tag`
+if [ -n "$TAGFILES" ]; then
+	rm $TAGFILES
+fi
+
+# Run the CPack process:
 mkdir build
 cd build
 cmake .. -DCPACK_GENERATOR=ZIP -DUHD_RELEASE_MODE="$1" ..
 make package
 mv uhd-images*.zip ..
+cmake .. -DCPACK_GENERATOR=TGZ -DUHD_RELEASE_MODE="$1" ..
+make package
+
+# Move images to here and clean up after us:
+mv uhd-images*.tar.gz ..
 cd ..
 rm -r build
+rm images/*.tag
