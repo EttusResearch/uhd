0da9251e68606dc9cbb3e1d36914cfb95f890ada
Martin Braun <martin.braun@ettus.com>
2014-11-03 16:27:08 +0100

    transport: Made CHDR-related routines separate from VRT
    Our VRT routines have the option to switch, on the fly, between
    VRLP and CHDR. This adds new CHDR-specific (un-)packers, which
    can only work with CHDR.

--- host/include/uhd/transport/vrt_if_packet.hpp
+++ host/include/uhd/transport/vrt_if_packet.hpp
@@ -1,5 +1,5 @@
 //
-// Copyright 2010-2013 Ettus Research LLC
+// Copyright 2010-2014 Ettus Research LLC
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -52,9 +52,18 @@ namespace vrt{
         //packet type
         enum packet_type_t
         {
+            // VRT language:
             PACKET_TYPE_DATA      = 0x0,
             PACKET_TYPE_IF_EXT    = 0x1,
             PACKET_TYPE_CONTEXT   = 0x2, //extension context: has_sid = true
+
+            // CVITA language:
+            //PACKET_TYPE_DATA      = 0x0, // Data
+            PACKET_TYPE_FC        = 0x1, // Flow control
+            PACKET_TYPE_ACK       = 0x1, // Flow control (ack)
+            PACKET_TYPE_CMD       = 0x2, // Command
+            PACKET_TYPE_RESP      = 0x3, // Command response
+            PACKET_TYPE_ERROR     = 0x3, // Command response: Error (the EOB bit is raised in this case)
         } packet_type;
 
         //size fields
@@ -65,18 +74,46 @@ namespace vrt{
 
         //header fields
         size_t packet_count;
+        //! Asserted for start- or end-of-burst
         bool sob, eob;
+        //! This is asserted for command responses that are errors (CHDR only)
+        bool error;
 
         //optional fields
+        //! Stream ID (SID). See uhd::sid_t
         bool has_sid; boost::uint32_t sid;
+        //! Class ID.
         bool has_cid; boost::uint64_t cid;
+        //! Integer timestamp
         bool has_tsi; boost::uint32_t tsi;
+        //! Fractional timestamp
         bool has_tsf; boost::uint64_t tsf;
+        //! Trailer
         bool has_tlr; boost::uint32_t tlr;
     };
 
     /*!
      * Pack a vrt header from metadata (big endian format).
+     *
+     * \section vrt_pack_contract Packing contract
+     *
+     * \subsection Requirements:
+     * - packet_buff points to a valid address space with enough space to write
+     *   the entire buffer, regardless of its length. At the very least, it must
+     *   be able to hold an entire header.
+     * - `if_packet_info` has the following members set to correct values:
+     *   - `has_*` members all set accordingly
+     *   - For every true `has_*` member, the corresponding variable holds a valid
+     *     value (e.g. if `has_sid` is true, `sid` contains a valid SID)
+     *   - `num_payload_bytes` and `num_payload_words32` are both set to the correct values
+     *
+     * \subsection Result:
+     * - `packet_buff` now points to a valid header that can be sent over the transport
+     *   without further modification
+     * - The following members on `if_packet_info` are set:
+     *   - `num_header_words32`
+     *   - `num_packet_words32`
+     *
      * \param packet_buff memory to write the packed vrt header
      * \param if_packet_info the if packet info (read/write)
      */
@@ -87,6 +124,34 @@ namespace vrt{
 
     /*!
      * Unpack a vrt header to metadata (big endian format).
+     *
+     * \section vrt_unpack_contract Unpacking contract
+     *
+     * \subsection Requirements
+     * - `packet_buff` points to a readable address space with a
+     *   CHDR packet, starting at the header. `packet_buff[0]` *must* always
+     *   point to a valid first word of the header. This implies that num_packet_words32
+     *   must be at least 1.
+     * - `if_packet_info` has the following members set to correct values:
+     *   - `num_packet_words32`. This means all values `packet_buff[0]`
+     *     through `packet_buff[if_packet_info.num_packet_words32-1]` are
+     *     readable words from this packet.
+     *   - `link_type`
+     *
+     * \subsection Result
+     * - `if_packet_info` now has the following values set to correct values:
+     *   - `packet_type`
+     *   - `num_payload_bytes`
+     *   - `num_payload_words32`
+     *   - `num_header_words32`
+     *   - `has_*`
+     *   - `sob`, `eob`, `error`, `cid`, `sid` (if applicable)
+     *   - `tsf`, `tsi` (if applicable)
+     *
+     * \subsection Exceptions
+     * - If the header is invalid, but the requirements are still met,
+     *   will throw a uhd::value_error.
+     *
      * \param packet_buff memory to read the packed vrt header
      * \param if_packet_info the if packet info (read/write)
      */
@@ -97,6 +162,9 @@ namespace vrt{
 
     /*!
      * Pack a vrt header from metadata (little endian format).
+     *
+     * See \ref vrt_pack_contract.
+     *
      * \param packet_buff memory to write the packed vrt header
      * \param if_packet_info the if packet info (read/write)
      */
@@ -107,6 +175,9 @@ namespace vrt{
 
     /*!
      * Unpack a vrt header to metadata (little endian format).
+     *
+     * See \ref vrt_unpack_contract.
+     *
      * \param packet_buff memory to read the packed vrt header
      * \param if_packet_info the if packet info (read/write)
      */
@@ -124,6 +195,7 @@ namespace vrt{
         num_packet_words32(0),
         packet_count(0),
         sob(false), eob(false),
+        error(false),
         has_sid(false), sid(0),
         has_cid(false), cid(0),
         has_tsi(false), tsi(0),
---
+++ host/lib/transport/chdr.cpp
@@ -0,0 +1,182 @@
+//
+// Copyright 2014 Ettus Research LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include <uhd/transport/chdr.hpp>
+#include <uhd/utils/byteswap.hpp>
+#include <uhd/exception.hpp>
+
+//define the endian macros to convert integers
+#ifdef BOOST_BIG_ENDIAN
+    #define BE_MACRO(x) (x)
+    #define LE_MACRO(x) uhd::byteswap(x)
+#else
+    #define BE_MACRO(x) uhd::byteswap(x)
+    #define LE_MACRO(x) (x)
+#endif
+
+using namespace uhd::transport::vrt;
+
+static const boost::uint32_t HDR_FLAG_TSF = (1 << 29);
+static const boost::uint32_t HDR_FLAG_EOB = (1 << 28);
+static const boost::uint32_t HDR_FLAG_ERROR = (1 << 28);
+
+/***************************************************************************/
+/* Packing                                                                 */
+/***************************************************************************/
+/*! Translate the contents of \p if_packet_info into a 32-Bit word and return it.
+ */
+UHD_INLINE boost::uint32_t _hdr_pack_chdr(
+        if_packet_info_t &if_packet_info
+) {
+    // Set fields in if_packet_info
+    if_packet_info.num_header_words32 = 2 + (if_packet_info.has_tsf ? 2 : 0);
+    if_packet_info.num_packet_words32 =
+            if_packet_info.num_header_words32 +
+            if_packet_info.num_payload_words32;
+
+    boost::uint16_t pkt_length =
+        if_packet_info.num_payload_bytes + (4 * if_packet_info.num_header_words32);
+    boost::uint32_t chdr = 0
+        // 2 Bits: Packet type
+        | (if_packet_info.packet_type << 30)
+        // 1 Bit: Has time
+        | (if_packet_info.has_tsf ? HDR_FLAG_TSF : 0)
+        // 1 Bit: EOB or Error
+        | ((if_packet_info.eob or if_packet_info.error) ? HDR_FLAG_EOB : 0)
+        // 12 Bits: Sequence number
+        | ((if_packet_info.packet_count & 0xFFF) << 16)
+        // 16 Bits: Total packet length
+        | pkt_length;
+    return chdr;
+}
+
+void chdr::if_hdr_pack_be(
+        boost::uint32_t *packet_buff,
+        if_packet_info_t &if_packet_info
+) {
+    // Write header and update if_packet_info
+    packet_buff[0] = BE_MACRO(_hdr_pack_chdr(if_packet_info));
+
+    // Write SID
+    packet_buff[1] = BE_MACRO(if_packet_info.sid);
+
+    // Write time
+    if (if_packet_info.has_tsf) {
+        packet_buff[2] = BE_MACRO(boost::uint32_t(if_packet_info.tsf >> 32));
+        packet_buff[3] = BE_MACRO(boost::uint32_t(if_packet_info.tsf >> 0));
+    }
+}
+
+void chdr::if_hdr_pack_le(
+        boost::uint32_t *packet_buff,
+        if_packet_info_t &if_packet_info
+) {
+    // Write header and update if_packet_info
+    packet_buff[0] = LE_MACRO(_hdr_pack_chdr(if_packet_info));
+
+    // Write SID
+    packet_buff[1] = LE_MACRO(if_packet_info.sid);
+
+    // Write time
+    if (if_packet_info.has_tsf) {
+        packet_buff[2] = LE_MACRO(boost::uint32_t(if_packet_info.tsf >> 32));
+        packet_buff[3] = LE_MACRO(boost::uint32_t(if_packet_info.tsf >> 0));
+    }
+}
+
+
+/***************************************************************************/
+/* Unpacking                                                               */
+/***************************************************************************/
+UHD_INLINE void _hdr_unpack_chdr(
+        const boost::uint32_t chdr,
+        if_packet_info_t &if_packet_info
+) {
+    // Set constant members
+    if_packet_info.link_type = if_packet_info_t::LINK_TYPE_CHDR;
+    if_packet_info.has_cid = false;
+    if_packet_info.has_sid = true;
+    if_packet_info.has_tsi = false;
+    if_packet_info.has_tlr = false;
+    if_packet_info.sob = false;
+
+    // Set configurable members
+    if_packet_info.has_tsf = bool(chdr & HDR_FLAG_TSF);
+    if_packet_info.packet_type = if_packet_info_t::packet_type_t((chdr >> 30) & 0x3);
+    if_packet_info.eob = (if_packet_info.packet_type == if_packet_info_t::PACKET_TYPE_DATA)
+                         && bool(chdr & HDR_FLAG_EOB);
+    if_packet_info.error = (if_packet_info.packet_type == if_packet_info_t::PACKET_TYPE_RESP)
+                         && bool(chdr & HDR_FLAG_ERROR);
+    if_packet_info.packet_count = (chdr >> 16) & 0xFFF;
+
+    // Set packet length variables
+    if (if_packet_info.has_tsf) {
+        if_packet_info.num_header_words32 = 4;
+    } else {
+        if_packet_info.num_header_words32 = 2;
+    }
+    size_t pkt_size_bytes = (chdr & 0xFFFF);
+    size_t pkt_size_word32 = (pkt_size_bytes / 4) + ((pkt_size_bytes % 4) ? 1 : 0);
+    // Check lengths match:
+    if (pkt_size_word32 < if_packet_info.num_header_words32) {
+        throw uhd::value_error("Bad CHDR or invalid packet length");
+    }
+    if (if_packet_info.num_packet_words32 < pkt_size_word32) {
+        throw uhd::value_error("Bad CHDR or packet fragment");
+    }
+    if_packet_info.num_payload_bytes = pkt_size_bytes - (4 * if_packet_info.num_header_words32);
+    if_packet_info.num_payload_words32 = pkt_size_word32 - if_packet_info.num_header_words32;
+}
+
+void chdr::if_hdr_unpack_be(
+        const boost::uint32_t *packet_buff,
+        if_packet_info_t &if_packet_info
+) {
+    // Read header and update if_packet_info
+    boost::uint32_t chdr = BE_MACRO(packet_buff[0]);
+    _hdr_unpack_chdr(chdr, if_packet_info);
+
+    // Read SID
+    if_packet_info.sid = BE_MACRO(packet_buff[1]);
+
+    // Read time (has_tsf was updated earlier)
+    if (if_packet_info.has_tsf) {
+        if_packet_info.tsf = 0
+            | boost::uint64_t(BE_MACRO(packet_buff[2])) << 32
+            | BE_MACRO(packet_buff[3]);
+    }
+}
+
+void chdr::if_hdr_unpack_le(
+        const boost::uint32_t *packet_buff,
+        if_packet_info_t &if_packet_info
+) {
+    // Read header and update if_packet_info
+    boost::uint32_t chdr = LE_MACRO(packet_buff[0]);
+    _hdr_unpack_chdr(chdr, if_packet_info);
+
+    // Read SID
+    if_packet_info.sid = LE_MACRO(packet_buff[1]);
+
+    // Read time (has_tsf was updated earlier)
+    if (if_packet_info.has_tsf) {
+        if_packet_info.tsf = 0
+            | boost::uint64_t(LE_MACRO(packet_buff[2])) << 32
+            | LE_MACRO(packet_buff[3]);
+    }
+}
+
