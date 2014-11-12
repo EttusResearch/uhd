2a49dbbc8acdd601eb4d541b43bcfa6724333785
Martin Braun <martin.braun@ettus.com>
2014-11-12 18:38:16 +0100

    uhd: Added endianness type

---
+++ host/include/uhd/types/endianness.hpp
@@ -0,0 +1,32 @@
+//
+// Copyright 2014 Ettus Research LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef INCLUDED_UHD_TYPES_ENDIANNESS_HPP
+#define INCLUDED_UHD_TYPES_ENDIANNESS_HPP
+
+#include <uhd/config.hpp>
+
+namespace uhd{
+
+    enum endianness_t {
+        ENDIANNESS_BIG,
+        ENDIANNESS_LITTLE
+    };
+
+} //namespace uhd
+
+#endif /* INCLUDED_UHD_TYPES_ENDIANNESS_HPP */
