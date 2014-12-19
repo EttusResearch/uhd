2ce97f6c05f607846eb11e9799b5d5045049e747
Brooks <brooks@ni>
2014-12-19 10:51:05 -0600

    x300: support new 120 MHz bandwidth versions of the NI-branded X310s

--- host/lib/transport/nirio/rpc/rpc_client.cpp
+++ host/lib/transport/nirio/rpc/rpc_client.cpp
@@ -52,7 +52,7 @@ rpc_client::rpc_client (
         //- address_configured: Only return addresses if a non-loopback address is configured for the system.
         //- numeric_host: No name resolution should be attempted for host
         //- numeric_service: No name resolution should be attempted for service
-        tcp::resolver::query::flags query_flags;
+        tcp::resolver::query::flags query_flags = tcp::resolver::query::passive;
         tcp::resolver::query query(tcp::v4(), server, port, query_flags);
         tcp::resolver::iterator iterator = resolver.resolve(query);
 
