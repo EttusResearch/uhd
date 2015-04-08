1942e608d8088b380b6aebee6a4f55b5942800bc
Michael Dickens <michael.dickens@ettus.com>
2015-04-08 13:11:23 -0400

    tests: fix use of 'uint32_t' to be in the correct namespace 'boost::'.

--- host/tests/sid_t_test.cpp
+++ host/tests/sid_t_test.cpp
@@ -30,12 +30,12 @@ BOOST_AUTO_TEST_CASE(test_sid_t) {
     BOOST_CHECK_EQUAL(sid.is_set(), true);
     BOOST_CHECK_EQUAL(sid.to_pp_string(), "1.2>3.16");
     BOOST_CHECK_EQUAL(sid.to_pp_string_hex(), "01:02>03:10");
-    BOOST_CHECK_EQUAL(sid.get_src(), (uint32_t)0x0102);
-    BOOST_CHECK_EQUAL(sid.get_dst(), (uint32_t)0x0310);
-    BOOST_CHECK_EQUAL(sid.get_src_addr(), (uint32_t)0x01);
-    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (uint32_t)0x02);
-    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (uint32_t)0x03);
-    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (uint32_t)0x10);
+    BOOST_CHECK_EQUAL(sid.get_src(), (boost::uint32_t)0x0102);
+    BOOST_CHECK_EQUAL(sid.get_dst(), (boost::uint32_t)0x0310);
+    BOOST_CHECK_EQUAL(sid.get_src_addr(), (boost::uint32_t)0x01);
+    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (boost::uint32_t)0x02);
+    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (boost::uint32_t)0x03);
+    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (boost::uint32_t)0x10);
     BOOST_CHECK_EQUAL(sid == sid, true);
     BOOST_CHECK_EQUAL(sid == sid_value, true);
 
@@ -68,88 +68,88 @@ BOOST_AUTO_TEST_CASE(test_sid_t_set) {
     sid_t sid(sid_value);
 
     sid.set(0x01020304);
-    BOOST_CHECK_EQUAL(sid.get(), (uint32_t)0x01020304);
-    BOOST_CHECK_EQUAL(sid.get_src_addr(),(uint32_t)0x01);
-    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (uint32_t)0x02);
-    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (uint32_t)0x03);
-    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (uint32_t)0x04);
-    BOOST_CHECK_EQUAL(sid.get_dst_xbarport(), (uint32_t)0x0);
-    BOOST_CHECK_EQUAL(sid.get_dst_blockport(), (uint32_t)0x4);
+    BOOST_CHECK_EQUAL(sid.get(), (boost::uint32_t)0x01020304);
+    BOOST_CHECK_EQUAL(sid.get_src_addr(),(boost::uint32_t)0x01);
+    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (boost::uint32_t)0x02);
+    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (boost::uint32_t)0x03);
+    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (boost::uint32_t)0x04);
+    BOOST_CHECK_EQUAL(sid.get_dst_xbarport(), (boost::uint32_t)0x0);
+    BOOST_CHECK_EQUAL(sid.get_dst_blockport(), (boost::uint32_t)0x4);
 
     sid.set_src_addr(0x0a);
-    BOOST_CHECK_EQUAL(sid.get(), (uint32_t)0x0a020304);
-    BOOST_CHECK_EQUAL(sid.get_src_addr(), (uint32_t)0x0a);
-    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (uint32_t)0x02);
-    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (uint32_t)0x03);
-    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (uint32_t)0x04);
+    BOOST_CHECK_EQUAL(sid.get(), (boost::uint32_t)0x0a020304);
+    BOOST_CHECK_EQUAL(sid.get_src_addr(), (boost::uint32_t)0x0a);
+    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (boost::uint32_t)0x02);
+    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (boost::uint32_t)0x03);
+    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (boost::uint32_t)0x04);
 
     sid.set_src_endpoint(0x0b);
-    BOOST_CHECK_EQUAL(sid.get(), (uint32_t)0x0a0b0304);
-    BOOST_CHECK_EQUAL(sid.get_src_addr(), (uint32_t)0x0a);
-    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (uint32_t)0x0b);
-    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (uint32_t)0x03);
-    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (uint32_t)0x04);
+    BOOST_CHECK_EQUAL(sid.get(), (boost::uint32_t)0x0a0b0304);
+    BOOST_CHECK_EQUAL(sid.get_src_addr(), (boost::uint32_t)0x0a);
+    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (boost::uint32_t)0x0b);
+    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (boost::uint32_t)0x03);
+    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (boost::uint32_t)0x04);
 
     sid.set_dst_addr(0x0c);
-    BOOST_CHECK_EQUAL(sid.get(), (uint32_t)0x0a0b0c04);
-    BOOST_CHECK_EQUAL(sid.get_src_addr(), (uint32_t)0x0a);
-    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (uint32_t)0x0b);
-    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (uint32_t)0x0c);
-    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (uint32_t)0x04);
+    BOOST_CHECK_EQUAL(sid.get(), (boost::uint32_t)0x0a0b0c04);
+    BOOST_CHECK_EQUAL(sid.get_src_addr(), (boost::uint32_t)0x0a);
+    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (boost::uint32_t)0x0b);
+    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (boost::uint32_t)0x0c);
+    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (boost::uint32_t)0x04);
 
     sid.set_dst_endpoint(0x0d);
-    BOOST_CHECK_EQUAL(sid.get(), (uint32_t)0x0a0b0c0d);
-    BOOST_CHECK_EQUAL(sid.get_src_addr(), (uint32_t)0x0a);
-    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (uint32_t)0x0b);
-    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (uint32_t)0x0c);
-    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (uint32_t)0x0d);
+    BOOST_CHECK_EQUAL(sid.get(), (boost::uint32_t)0x0a0b0c0d);
+    BOOST_CHECK_EQUAL(sid.get_src_addr(), (boost::uint32_t)0x0a);
+    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (boost::uint32_t)0x0b);
+    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (boost::uint32_t)0x0c);
+    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (boost::uint32_t)0x0d);
 
     sid.set_dst_xbarport(0xb);
-    BOOST_CHECK_EQUAL(sid.get(), (uint32_t)0x0a0b0cbd);
-    BOOST_CHECK_EQUAL(sid.get_src_addr(), (uint32_t)0x0a);
-    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (uint32_t)0x0b);
-    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (uint32_t)0x0c);
-    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (uint32_t)0xbd);
+    BOOST_CHECK_EQUAL(sid.get(), (boost::uint32_t)0x0a0b0cbd);
+    BOOST_CHECK_EQUAL(sid.get_src_addr(), (boost::uint32_t)0x0a);
+    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (boost::uint32_t)0x0b);
+    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (boost::uint32_t)0x0c);
+    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (boost::uint32_t)0xbd);
 
     sid.set_dst_blockport(0xc);
-    BOOST_CHECK_EQUAL(sid.get(), (uint32_t)0x0a0b0cbc);
-    BOOST_CHECK_EQUAL(sid.get_src_addr(), (uint32_t)0x0a);
-    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (uint32_t)0x0b);
-    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (uint32_t)0x0c);
-    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (uint32_t)0xbc);
+    BOOST_CHECK_EQUAL(sid.get(), (boost::uint32_t)0x0a0b0cbc);
+    BOOST_CHECK_EQUAL(sid.get_src_addr(), (boost::uint32_t)0x0a);
+    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (boost::uint32_t)0x0b);
+    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (boost::uint32_t)0x0c);
+    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (boost::uint32_t)0xbc);
 
     sid_t flipped_sid = sid.reversed();
-    BOOST_CHECK_EQUAL(flipped_sid.get(), (uint32_t)0x0cbc0a0b);
+    BOOST_CHECK_EQUAL(flipped_sid.get(), (boost::uint32_t)0x0cbc0a0b);
 
     // In-place
     sid.reverse();
-    BOOST_CHECK_EQUAL(sid.get(), (uint32_t)0x0cbc0a0b);
+    BOOST_CHECK_EQUAL(sid.get(), (boost::uint32_t)0x0cbc0a0b);
 }
 
 BOOST_AUTO_TEST_CASE(test_sid_t_from_str) {
     sid_t sid("1.2>3.4");
-    BOOST_CHECK_EQUAL(sid.get_src_addr(), (uint32_t)1);
-    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (uint32_t)2);
-    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (uint32_t)3);
-    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (uint32_t)4);
+    BOOST_CHECK_EQUAL(sid.get_src_addr(), (boost::uint32_t)1);
+    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (boost::uint32_t)2);
+    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (boost::uint32_t)3);
+    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (boost::uint32_t)4);
 
     sid = "01:02>03:10";
-    BOOST_CHECK_EQUAL(sid.get_src_addr(), (uint32_t)1);
-    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (uint32_t)2);
-    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (uint32_t)3);
-    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (uint32_t)16);
+    BOOST_CHECK_EQUAL(sid.get_src_addr(), (boost::uint32_t)1);
+    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (boost::uint32_t)2);
+    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (boost::uint32_t)3);
+    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (boost::uint32_t)16);
 
     sid = "01:06/03:10";
-    BOOST_CHECK_EQUAL(sid.get_src_addr(), (uint32_t)1);
-    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (uint32_t)6);
-    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (uint32_t)3);
-    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (uint32_t)16);
+    BOOST_CHECK_EQUAL(sid.get_src_addr(), (boost::uint32_t)1);
+    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (boost::uint32_t)6);
+    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (boost::uint32_t)3);
+    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (boost::uint32_t)16);
 
     sid = "01:02:04:10";
-    BOOST_CHECK_EQUAL(sid.get_src_addr(), (uint32_t)1);
-    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (uint32_t)2);
-    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (uint32_t)4);
-    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (uint32_t)16);
+    BOOST_CHECK_EQUAL(sid.get_src_addr(), (boost::uint32_t)1);
+    BOOST_CHECK_EQUAL(sid.get_src_endpoint(), (boost::uint32_t)2);
+    BOOST_CHECK_EQUAL(sid.get_dst_addr(), (boost::uint32_t)4);
+    BOOST_CHECK_EQUAL(sid.get_dst_endpoint(), (boost::uint32_t)16);
 
     BOOST_REQUIRE_THROW(sid_t fail_sid("foobar"), uhd::value_error);
     BOOST_REQUIRE_THROW(sid_t fail_sid("01:02:03:4"), uhd::value_error);
