64990acea741768becafb3e9dee171274f23f9d9
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2015-04-24 15:53:43 -0700

    e300: Reduced AD9361's RX data delay to improve timing in FPGA capture interface

--- host/lib/usrp/e300/e300_defaults.hpp
+++ host/lib/usrp/e300/e300_defaults.hpp
@@ -70,7 +70,7 @@ public:
     digital_interface_delays_t get_digital_interface_timing() {
         digital_interface_delays_t delays;
         delays.rx_clk_delay = 0;
-        delays.rx_data_delay = 0xF;
+        delays.rx_data_delay = 0x8;
         delays.tx_clk_delay = 0;
         delays.tx_data_delay = 0xF;
         return delays;
