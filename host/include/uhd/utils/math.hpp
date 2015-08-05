3dbced2df2942fd8ec4dffead8345a9b9ce7faf0
Nicholas Corgan <nick.corgan@ettus.com>
2015-08-05 09:58:53 -0700

    Fixed .ipp include behavior
    * Use full include path in public headers, not quotes
    * Install .ipp files alongside corresponding .hpp

--- host/include/uhd/utils/math.hpp
+++ host/include/uhd/utils/math.hpp
@@ -1,5 +1,5 @@
 //
-// Copyright 2014 Ettus Research LLC
+// Copyright 2014-2015 Ettus Research LLC
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -240,7 +240,7 @@ namespace fp_compare {
 } // namespace math
 } // namespace uhd
 
-#include "fp_compare_epsilon.ipp"
-#include "fp_compare_delta.ipp"
+#include <uhd/utils/fp_compare_epsilon.ipp>
+#include <uhd/utils/fp_compare_delta.ipp>
 
 #endif /* INCLUDED_UHD_UTILS_MATH_HPP */
