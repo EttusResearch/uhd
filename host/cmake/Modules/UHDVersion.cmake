f7a15853324e3e0001c7a9e5bab397cb24e0e01f
Martin Braun <martin.braun@ettus.com>
2015-09-11 14:18:43 -0700

    Updated repo for 3.9.1 release
    - Version string
    - Renamed images package

--- host/cmake/Modules/UHDVersion.cmake
+++ host/cmake/Modules/UHDVersion.cmake
@@ -28,7 +28,7 @@ FIND_PACKAGE(Git QUIET)
 ########################################################################
 SET(UHD_VERSION_MAJOR 003)
 SET(UHD_VERSION_MINOR 009)
-SET(UHD_VERSION_PATCH 000)
+SET(UHD_VERSION_PATCH 001)
 SET(UHD_VERSION_DEVEL FALSE)
 
 ########################################################################
