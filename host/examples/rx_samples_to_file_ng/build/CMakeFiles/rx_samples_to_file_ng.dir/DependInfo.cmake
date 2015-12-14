33a1ef838cc9436d7e1f89e1cbb63d1f9526ccde
jdesalas <jdesalas777@gmail.com>
2015-12-14 09:13:49 +0100

    Adding support for sc8 as a storing format in rx_samples_to_file_ng

---
+++ host/examples/rx_samples_to_file_ng/build/CMakeFiles/rx_samples_to_file_ng.dir/DependInfo.cmake
@@ -0,0 +1,18 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jdesalas/uhd-ng/uhd-ng/host/examples/rx_samples_to_file_ng/rx_samples_to_file_ng.cpp" "/Users/jdesalas/uhd-ng/uhd-ng/host/examples/rx_samples_to_file_ng/build/CMakeFiles/rx_samples_to_file_ng.dir/rx_samples_to_file_ng.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ host/examples/rx_samples_to_file_ng/build/CMakeFiles/rx_samples_to_file_ng.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rx_samples_to_file_ng.dir/rx_samples_to_file_ng.cpp.o"
+  "rx_samples_to_file_ng.pdb"
+  "rx_samples_to_file_ng"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rx_samples_to_file_ng.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
