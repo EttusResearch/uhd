6c79e6d14d1631085515d86140df5f2a9522efc7
Martin Braun <martin.braun@ettus.com>
2016-08-01 21:21:26 -0700

    x300: Updated copyright year

--- host/lib/usrp/x300/x300_impl.cpp
+++ host/lib/usrp/x300/x300_impl.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright 2013-2015 Ettus Research LLC
+// Copyright 2013-2016 Ettus Research LLC
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
