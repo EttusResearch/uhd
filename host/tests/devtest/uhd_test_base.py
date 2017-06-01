9ec36e9538b4be2c6e6e77aa551bd236ef753a10
Trung N Tran <trung.tran@ettus.com>
2017-06-01 15:28:19 -0700

    RTS: DevTest failure work around by increasing time between test

--- host/tests/devtest/uhd_test_base.py
+++ host/tests/devtest/uhd_test_base.py
@@ -92,7 +92,7 @@ class uhd_test_case(unittest.TestCase):
         self.tear_down()
         if self.results_file:
             open(self.results_file, 'w').write(yaml.dump(self.results, default_flow_style=False))
-
+        time.sleep(15)
     def report_result(self, testname, key, value):
         """ Store a result as a key/value pair.
         After completion, all results for one test are written to the results file.
