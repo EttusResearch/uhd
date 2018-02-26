82100306d0b7ba6c7483853c3e7bcc54619eae8e
Trung N Tran <trung.tran@ettus.com>
2018-02-26 12:21:43 -0800

    Revert "utils: images downloader: forced update of CMake variable"
    This reverts commit cf1bf696649c958c53d7f4cf0a280242c254e4be.

--- host/cmake/Modules/UHDImagesDownloader.cmake
+++
@@ -1,6 +0,0 @@
-#UHD images downloader configuration
-FILE(READ ${CURRENT_DIR}/../../images/manifest.txt CMAKE_MANIFEST_CONTENTS)
-CONFIGURE_FILE(
-    ${CURRENT_DIR}/uhd_images_downloader.py.in
-    ${INSTALL_DIR}/uhd_images_downloader.py
-@ONLY)
