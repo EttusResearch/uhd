46679b563cf426c9f9ed35fa6a4870a664323260
Moritz Fischer <moritz.fischer@ettus.com>
2018-04-25 18:01:09 -0700

    mpm: tools: Introduce dt-compat and mcu-compat fields
    Introduce dt-compat and mcu-compat fields into the eeprom
    structure.
    For the motherboard eeprom this is straightforward, since
    there's still padding bytes that could be (ab)used for this.
    On the dboard side more creativity is required and the
    original revision field of 2 bytes is reduced to only
    one byte revision and one byte dt-compat.
    Since this will only affect new units being backwards
    compatible with older versions of the bootloader is not
    an issue.
    
    Reviewed-by: Brent Stapleton <brent.stapleton@ettus.com>
    Signed-off-by: Moritz Fischer <moritz.fischer@ettus.com>

--- mpm/tools/db-init.c
+++ mpm/tools/db-init.c
@@ -8,14 +8,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int get_dt_compat(int rev)
+{
+	if (rev > 3)
+		return 3;
+
+	return rev;
+}
+
 void usage(char *argv[])
 {
 	printf("-- Usage -- \n");
-	printf("%s slot pid rev serial#\n\n", argv[0]);
+	printf("%s slot pid rev serial [dt-compat]#\n\n", argv[0]);
 	printf("Example:\n");
 	printf("$ %s 0 0x0150 0 310A850\n",
 		argv[0]);
-
+	printf("or specifying a dt-compat explicitly:\n");
+	printf("$ %s 0 0x0150 0 310A850 3\n",
+		argv[0]);
 }
 
 
@@ -23,15 +33,20 @@ int main(int argc, char *argv[])
 {
 	struct usrp_sulfur_db_eeprom *ep;
 	int which_slot = 0;
+	u8 dt_compat = 0;
 
-	if (argc != 5) {
+	if (argc < 5) {
 		usage(argv);
 		return EXIT_FAILURE;
 	}
 
+	if (argc >= 6)
+		dt_compat = atoi(argv[5]);
+
 	which_slot = atoi(argv[1]);
 
-	ep = usrp_sulfur_db_eeprom_new(strtol(argv[2], NULL, 16), atoi(argv[3]), argv[4]);
+	ep = usrp_sulfur_db_eeprom_new(strtol(argv[2], NULL, 16), atoi(argv[3]), argv[4],
+			dt_compat ? dt_compat : get_dt_compat(atoi(argv[3])));
 	usrp_sulfur_db_eeprom_print(ep);
 
 	if (!which_slot)
