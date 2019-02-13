55c6222579a01efd55e7ade3938031f9d5d0bfbd
Martin Braun <martin.braun@ettus.com>
2019-02-13 15:01:33 -0800

    cmake: Update version string to 3.15.0.git

--- host/cmake/Modules/UHDVersion.cmake
+++ host/cmake/Modules/UHDVersion.cmake
@@ -17,11 +17,11 @@ find_package(Git QUIET)
 #  - Increment patch for bugfixes and docs
 #  - set UHD_VERSION_DEVEL to true for master and development branches
 ########################################################################
-set(UHD_VERSION_MAJOR   3)
-set(UHD_VERSION_API    14)
-set(UHD_VERSION_ABI     0)
-set(UHD_VERSION_PATCH   0)
-set(UHD_VERSION_DEVEL FALSE)
+set(UHD_VERSION_MAJOR 3)
+set(UHD_VERSION_API   15)
+set(UHD_VERSION_ABI   0)
+set(UHD_VERSION_PATCH git)
+set(UHD_VERSION_DEVEL TRUE)
 
 ########################################################################
 # If we're on a development branch, we skip the patch version
