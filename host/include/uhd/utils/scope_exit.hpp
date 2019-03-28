beb1f3c207bcf1ebc6ca1e83f1940bd91c867d9a
Martin Braun <martin.braun@ettus.com>
2019-03-27 17:32:09 -0700

    utils: Add scope_exit object
    This is a utility for RAII-style operations. An object that will run
    code when a scope is left.
    
    Also includes unit tests.

---
+++ host/include/uhd/utils/scope_exit.hpp
@@ -0,0 +1,55 @@
+//
+// Copyright 2019 Ettus Research, a National Instruments Brand
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+
+#ifndef INCLUDED_UHD_SCOPE_EXIT_HPP
+#define INCLUDED_UHD_SCOPE_EXIT_HPP
+
+#include <functional>
+#include <memory>
+
+namespace uhd { namespace utils {
+
+/*! A class that will execute a function on its destruction
+ *
+ * Similar to Boost.ScopeExit. A useful tool for RAII-style operations.
+ *
+ * Note: The creation of the object can be costly if converting the exit
+ * callback to exit_cb_t incurs copying overhead. Keep this in mind when using
+ * this object in a high-performance path.
+ */
+class scope_exit
+{
+public:
+    using uptr = std::unique_ptr<scope_exit>;
+    using exit_cb_t = std::function<void(void)>;
+
+    // \param exit_b The function object ("exit callback") that gets executed
+    //               in the destructor
+    static uptr make(exit_cb_t&& exit_cb)
+    {
+        // When we have C++14, use make_unique instead (TODO)
+        return uptr(new scope_exit(std::forward<exit_cb_t>(exit_cb)));
+    }
+
+    ~scope_exit()
+    {
+        _exit_cb();
+    }
+
+private:
+    scope_exit(std::function<void(void)>&& exit_cb)
+        : _exit_cb(std::forward<std::function<void(void)>>(exit_cb))
+    {
+        // nop
+    }
+
+    std::function<void(void)> _exit_cb;
+};
+
+}} /* namespace uhd::rfnoc */
+
+#endif /* INCLUDED_UHD_SCOPE_EXIT_HPP */
+
---
+++ host/tests/scope_exit_test.cpp
@@ -0,0 +1,45 @@
+//
+// Copyright 2019 Ettus Research, a National Instruments Brand
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+
+#include <uhd/utils/scope_exit.hpp>
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+
+BOOST_AUTO_TEST_CASE(test_scope_exit)
+{
+    bool flag = false;
+    {
+        auto flag_setter = uhd::utils::scope_exit::make([&flag]() { flag = true; });
+        BOOST_CHECK(!flag);
+    }
+    BOOST_CHECK(flag);
+}
+
+BOOST_AUTO_TEST_CASE(test_scope_exit_function_object)
+{
+    bool flag = false;
+    std::function<void(void)> resetter = [&flag]() { flag = true; };
+
+    {
+        auto flag_setter = uhd::utils::scope_exit::make(std::move(resetter));
+        BOOST_CHECK(!flag);
+    }
+    BOOST_CHECK(flag);
+}
+
+BOOST_AUTO_TEST_CASE(test_scope_exit_function_named_lambda)
+{
+    bool flag = false;
+    auto resetter = [&flag]() { flag = true; };
+
+    {
+        // Note: Does not require std::move()
+        auto flag_setter = uhd::utils::scope_exit::make(resetter);
+        BOOST_CHECK(!flag);
+    }
+    BOOST_CHECK(flag);
+}
+
