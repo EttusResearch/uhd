6f3c201a802079a3d565c5f14e1222743097b459
Martin Braun <martin.braun@ettus.com>
2019-06-20 16:38:06 -0700

    uhdlib: transport: Add links.hpp
    This contains both_links_t

---
+++ host/lib/include/uhdlib/transport/links.hpp
@@ -0,0 +1,43 @@
+//
+// Copyright 2019 Ettus Research, a National Instruments Brand
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+
+#ifndef INCLUDED_UHDLIB_TRANSPORT_LINKS_HPP
+#define INCLUDED_UHDLIB_TRANSPORT_LINKS_HPP
+
+#include <uhdlib/transport/io_service.hpp>
+#include <uhdlib/transport/link_if.hpp>
+#include <tuple>
+
+namespace uhd { namespace transport {
+
+enum class link_type_t { CTRL = 0, ASYNC_MSG, TX_DATA, RX_DATA };
+
+//! Contains all information regarding a link interface
+using both_links_t = std::tuple<uhd::transport::io_service::sptr,
+    uhd::transport::send_link_if::sptr,
+    size_t, // num_send_frames
+    uhd::transport::recv_link_if::sptr,
+    size_t, // num_recv_frames
+    bool>;  // lossy_xport
+
+/*!
+ * Parameters for link creation.
+ */
+struct link_params_t
+{
+    size_t recv_frame_size = 0;
+    size_t send_frame_size = 0;
+    size_t num_recv_frames = 0;
+    size_t num_send_frames = 0;
+    size_t recv_buff_size  = 0;
+    size_t send_buff_size  = 0;
+};
+
+
+}} // namespace uhd::transport
+
+#endif /* INCLUDED_UHDLIB_TRANSPORT_LINKS_HPP */
+
