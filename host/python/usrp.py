a3237b6baccecfeb44aeb85d409f8c2d93acab74
iprivit <41305661+iprivit@users.noreply.github.com>
2019-10-28 13:58:11 -0400

    Update usrp.py MultiUSRP class
    The send_waveform function takes the waveform_proto array and if it has only 1 channel, explicitly reshapes it to be (1, waveform_proto.size), or uses np.tile to replicate the waveform_proto array over X channels. It then proceeds to loop over the waveform_proto array, but attempts to do so over the channel dimension instead of looping over the actual samples. This results in sending the entire waveform_proto array regardless of the duration specified.

--- host/python/usrp.py
+++ host/python/usrp.py
@@ -116,7 +116,7 @@ class MultiUSRP(lib.usrp.multi_usrp):
         while send_samps < max_samps:
             real_samps = min(proto_len, max_samps-send_samps)
             if real_samps < proto_len:
-                samples = streamer.send(waveform_proto[:real_samps], metadata)
+                samples = streamer.send(waveform_proto[:,:real_samps], metadata)
             else:
                 samples = streamer.send(waveform_proto, metadata)
             send_samps += samples
