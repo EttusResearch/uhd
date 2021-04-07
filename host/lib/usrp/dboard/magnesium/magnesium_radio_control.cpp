d3da5e4cda3f75649381788014b214e0727ef5d1
michael-west <michael.west@ettus.com>
2021-04-07 10:54:57 -0700

    N310: Fix calculation of LO freq w/ext LO for RX
    A typo was causing an issue with order of precedence in the calculation
    of the LO frequency when using an external LO for RX on the N310.
    
    Signed-off-by: michael-west <michael.west@ettus.com>

--- host/lib/usrp/dboard/magnesium/magnesium_radio_control.cpp
+++ host/lib/usrp/dboard/magnesium/magnesium_radio_control.cpp
@@ -337,7 +337,7 @@ double magnesium_radio_control_impl::set_rx_frequency(
     }
     // external LO required to tune at 2xdesired_frequency.
     const double lo1_freq =
-        (ad9371_source == "internal" ? 1 : 2) * coerced_if_freq + freq;
+        (ad9371_source == "internal" ? 1 : 2) * (coerced_if_freq + freq);
 
     this->_set_rx_lo_freq(ad9371_source, MAGNESIUM_LO1, lo1_freq, chan);
 
