666ae09e5c9f0b7b8583b3ddcbd5b0ef1cdc2eaf
Michael Dickens <michael.dickens@ettus.com>
2021-05-14 11:15:34 -0400

    mpm: add FPGA type to the info returned during discovery
    Also update this info upon server reset, such as when a new FPGA image is loaded

--- mpm/python/usrp_mpm/discovery.py
+++ mpm/python/usrp_mpm/discovery.py
@@ -55,6 +55,7 @@ def _discovery_process(state, discovery_addr):
             [b"type="+state.dev_type.value] + \
             [b"product="+state.dev_product.value] + \
             [b"serial="+state.dev_serial.value] + \
+            [b"fpga="+state.dev_fpga_type.value] + \
             [RESPONSE_CLAIMED_KEY+to_binary_str("={}".format(state.claim_status.value))]
         )
 
