8edd13e6eba61ef2bfd96c7dc88b2642decbaa7a
Wade Fife <wade.fife@ettus.com>
2021-06-15 14:15:03 -0500

    fpga: lib: Clean up and document lib files
    Clean-up and document axi_tag_time, dds_freq_tune, and axi_sync.

--- fpga/usrp3/lib/rfnoc/axi_sync.v
+++ fpga/usrp3/lib/rfnoc/axi_sync.v
@@ -1,14 +1,32 @@
 //
-// Copyright 2016 Ettus Research
-// Copyright 2018 Ettus Research, a National Instruments Company
+// Copyright 2021 Ettus Research, a National Instruments Brand
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 //
-// Synchronizes AXI stream buses so data is released on every port simultaneously.
+// Module: axi_sync
 //
-// Note: If inputs have inequal bitwidths, use WIDTH_VEC instead of WIDTH to define
-//       the individual bit widths. Each bit width is defined with 8-bits stuffed
-//       into a vector of width 8*SIZE.
+// Description:
+//
+//   Synchronizes AXI stream buses so data is released on every port
+//   simultaneously. Multiple inputs/outputs are supported by concatenating the
+//   bus signals together. The number and size of each input/output bus is
+//   controlled using parameters.
+//
+//    **WARNING**: This module violates the AXI4-Stream specification by not
+//                 asserting TVALID until it receives TREADY. This will not
+//                 work if downstream logic waits for TVALID before asserting
+//                 TREADY, which is common. Use with care.
+//
+// Parameters:
+//
+//   SIZE      : The number of inputs streams to synchronize.
+//   WIDTH     : The width of TDATA on the input streams, if they are all the
+//               same width. If they are different widths, then use WIDTH_VEC
+//               instead.
+//   WIDTH_VEC : A vector of widths corresponding to each stream's TDATA width.
+//               Each number in this vector must be 32 bits wide. This defaults
+//               to WIDTH bits for all inputs.
+//   FIFO_SIZE : Log2 the size of the FIFO to use internally for each stream.
 //
 
 module axi_sync #(
@@ -16,48 +34,75 @@ module axi_sync #(
   parameter               WIDTH     = 32,
   parameter [32*SIZE-1:0] WIDTH_VEC = {SIZE{WIDTH[31:0]}},
   parameter               FIFO_SIZE = 0
-)(
-  input clk, input reset, input clear,
-  input [msb(SIZE,WIDTH_VEC)-1:0] i_tdata, input  [SIZE-1:0] i_tlast, input  [SIZE-1:0] i_tvalid, output [SIZE-1:0] i_tready,
-  output [msb(SIZE,WIDTH_VEC)-1:0] o_tdata, output [SIZE-1:0] o_tlast, output [SIZE-1:0] o_tvalid, input  [SIZE-1:0] o_tready
+) (
+  input  clk,
+  input  reset,
+  input  clear,
+
+  // Input streams
+  input  [len(SIZE)-1:0] i_tdata,
+  input  [     SIZE-1:0] i_tlast,
+  input  [     SIZE-1:0] i_tvalid,
+  output [     SIZE-1:0] i_tready,
+
+  // Output streams
+  output [len(SIZE)-1:0] o_tdata,
+  output [     SIZE-1:0] o_tlast,
+  output [     SIZE-1:0] o_tvalid,
+  input  [     SIZE-1:0] o_tready
 );
 
-  // Helper function to calculate the MSB index based on widths stored in WIDTH_VEC.
-  // Note: If n is negative, returns 0
-  function automatic integer msb(input integer n, input [SIZE*32-1:0] bit_vec);
+  // Helper function to calculate the combined length of the lower 'n' ports
+  // based on widths stored in WIDTH_VEC. Note: If n is negative, returns 0.
+  function automatic integer len(input integer n);
     integer i, total;
   begin
     total = 0;
     if (n >= 0) begin
       for (i = 0; i <= n; i = i + 1) begin
-        total = total + ((bit_vec >> 32*i) & 32'hFF);
+        total = total + ((WIDTH_VEC >> 32*i) & 32'hFFFF);
       end
     end
-    msb = total;
+    len = total;
   end
   endfunction
 
-  wire [msb(SIZE,WIDTH_VEC)-1:0] int_tdata;
-  wire [SIZE-1:0] int_tlast, int_tvalid, int_tready;
+  wire [len(SIZE)-1:0] int_tdata;
+  wire [     SIZE-1:0] int_tlast;
+  wire [     SIZE-1:0] int_tvalid;
+  wire [     SIZE-1:0] int_tready;
 
+  // Generate a FIFO for each stream
   genvar i;
   generate
     for (i = 0; i < SIZE; i = i + 1) begin
-      axi_fifo #(.WIDTH(msb(i,WIDTH_VEC)-msb(i-1,WIDTH_VEC)+1), .SIZE(FIFO_SIZE)) axi_fifo (
-        .clk(clk), .reset(reset), .clear(clear),
-        .i_tdata({i_tlast[i],i_tdata[msb(i,WIDTH_VEC)-1:msb(i-1,WIDTH_VEC)]}),
-        .i_tvalid(i_tvalid[i]), .i_tready(i_tready[i]),
-        .o_tdata({int_tlast[i],int_tdata[msb(i,WIDTH_VEC)-1:msb(i-1,WIDTH_VEC)]}),
-        .o_tvalid(int_tvalid[i]), .o_tready(int_tready[i]),
-        .space(), .occupied());
+      axi_fifo #(
+        .WIDTH (len(i)-len(i-1)+1),
+        .SIZE  (FIFO_SIZE)
+      ) axi_fifo (
+        .clk      (clk),
+        .reset    (reset),
+        .clear    (clear),
+        .i_tdata  ({ i_tlast[i], i_tdata[len(i)-1 : len(i-1)] }),
+        .i_tvalid (i_tvalid[i]),
+        .i_tready (i_tready[i]),
+        .o_tdata  ({ int_tlast[i], int_tdata[len(i)-1 : len(i-1)] }),
+        .o_tvalid (int_tvalid[i]),
+        .o_tready (int_tready[i]),
+        .space    (),
+        .occupied ()
+      );
     end
   endgenerate
 
-  assign o_tdata    = int_tdata;
-  assign o_tlast    = int_tlast;
-
+  // We allow a transfer and consume the outputs of the FIFOs when all
+  // downstream blocks are ready to accept a transfer (o_tready is true for all
+  // streams) and all FIFOs have data ready (int_tvalid is true for all FIFOs).
   wire consume = (&int_tvalid) & (&o_tready);
+
   assign int_tready = {SIZE{consume}};
   assign o_tvalid   = {SIZE{consume}};
+  assign o_tdata    = int_tdata;
+  assign o_tlast    = int_tlast;
 
 endmodule
--- fpga/usrp3/lib/rfnoc/axi_tag_time.v
+++ fpga/usrp3/lib/rfnoc/axi_tag_time.v
@@ -1,79 +1,115 @@
 //
-// Copyright 2016 Ettus Research
-// Copyright 2018 Ettus Research, a National Instruments Company
+// Copyright 2021 Ettus Research, a National Instruments Brand
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 //
-// - When the user executes a timed settings bus command,
-//   this module will tag the sample (on m_axis_data_tuser)
-//   that the command should apply on.
-// - Order of operation:
-//   1) Receives settings bus command
-//      a) If time != 0, output on non-timed settings bus
-//      b) If time != 0, output on timed settings bus and store time in FIFO
-//         It is assumed the user will use timed_settings_bus.v implementation.
-//   2) 
+// Module: axi_tag_time
+//
+// Description:
+//
+//   This module accepts samples on s_axis_data_* and outputs them on
+//   m_axis_data_*. When the user executes a timed settings bus command to a
+//   register, this module will tag the sample that the command should apply on
+//   by asserting the bit corresponding to that register in m_axis_data_tag.
+//
+//   The order of operations is as follows:
+//
+//   1) Receives settings bus command on in_set_*.
+//
+//      a) If in_set_has_time == 0, output on non-timed settings bus (out_set_*).
+//
+//      b) If in_set_has_time == 1, output on timed settings bus (timed_set_*)
+//         and store the time in a FIFO. It is assumed the user will use
+//         timed_settings_bus.v implementation.
+//
+//   2) When the sample corresponding to the time stored in the FIFO arrives on
+//      s_axis_data_*, assert the corresponding bit in m_axis_data_tag at the
+//      same time the sample is output on m_axis_data_*.
+//
+//   The TUSER port contains the packet header information:
+//
+//     tuser[125]  : Has timestamp
+//     tuser[63:0] : Timestamp
+//
+//   This field should be valid for the duration of the packet, although in
+//   reality the timestamp is only read during the first sample of each packet.
+//   The "has time" bit will be read at various points depending on the
+//   requested time.
+//
 
 module axi_tag_time #(
-  parameter WIDTH            = 32,
-  parameter HEADER_WIDTH     = 128,
-  parameter SR_AWIDTH        = 8,
-  parameter SR_DWIDTH        = 32,
-  parameter SR_TWIDTH        = 64,
-  parameter NUM_TAGS         = 1,
-  parameter [NUM_TAGS*SR_AWIDTH-1:0] SR_TAG_ADDRS = 0,
-  parameter CMD_FIFO_SIZE    = 5,
-  parameter MAX_TICK_RATE    = 2**16-1
-)(
+  parameter                          WIDTH         = 32,
+  parameter                          HEADER_WIDTH  = 128,
+  parameter                          SR_AWIDTH     = 8,
+  parameter                          SR_DWIDTH     = 32,
+  parameter                          SR_TWIDTH     = 64,
+  parameter                          NUM_TAGS      = 1,
+  parameter [NUM_TAGS*SR_AWIDTH-1:0] SR_TAG_ADDRS  = 0,
+  parameter                          CMD_FIFO_SIZE = 5,
+  parameter                          MAX_TICK_RATE = 2**16-1
+) (
   input clk,
   input reset,
   input clear,
-  input [$clog2(MAX_TICK_RATE)-1:0] tick_rate,
-  output timed_cmd_fifo_full,
-  // From AXI Wrapper
-  input [WIDTH-1:0] s_axis_data_tdata,
-  input [HEADER_WIDTH-1:0] s_axis_data_tuser,
-  input s_axis_data_tlast,
-  input s_axis_data_tvalid,
-  output s_axis_data_tready,
-  // To user
-  output [WIDTH-1:0] m_axis_data_tdata,
+
+  input  [$clog2(MAX_TICK_RATE)-1:0] tick_rate,
+  output                             timed_cmd_fifo_full,
+
+  // Input sample stream
+  input  [       WIDTH-1:0] s_axis_data_tdata,
+  input  [HEADER_WIDTH-1:0] s_axis_data_tuser,
+  input                     s_axis_data_tlast,
+  input                     s_axis_data_tvalid,
+  output                    s_axis_data_tready,
+
+  // Output sample string, with tag
+  output [       WIDTH-1:0] m_axis_data_tdata,
   output [HEADER_WIDTH-1:0] m_axis_data_tuser,
-  output [NUM_TAGS-1:0] m_axis_data_tag,
-  output m_axis_data_tlast,
-  output m_axis_data_tvalid,
-  input m_axis_data_tready,
-  // Settings bus from Noc Shell
-  input in_set_stb,
+  output [    NUM_TAGS-1:0] m_axis_data_tag,
+  output                    m_axis_data_tlast,
+  output                    m_axis_data_tvalid,
+  input                     m_axis_data_tready,
+
+  // Settings bus
+  input                 in_set_stb,
   input [SR_AWIDTH-1:0] in_set_addr,
   input [SR_DWIDTH-1:0] in_set_data,
   input [SR_TWIDTH-1:0] in_set_time,
-  input in_set_has_time,
+  input                 in_set_has_time,
+
   // Non-timed settings bus to user
-  output out_set_stb,
+  output                 out_set_stb,
   output [SR_AWIDTH-1:0] out_set_addr,
   output [SR_DWIDTH-1:0] out_set_data,
+
   // Timed settings bus to user
-  output timed_set_stb,
+  output                 timed_set_stb,
   output [SR_AWIDTH-1:0] timed_set_addr,
   output [SR_DWIDTH-1:0] timed_set_data
 );
 
-  assign out_set_addr = in_set_addr;
-  assign out_set_data = in_set_data;
-  assign out_set_stb  = in_set_stb & ~in_set_has_time;
+  assign out_set_addr   = in_set_addr;
+  assign out_set_data   = in_set_data;
+  assign out_set_stb    = in_set_stb & ~in_set_has_time;
+
   assign timed_set_addr = in_set_addr;
   assign timed_set_data = in_set_data;
   assign timed_set_stb  = in_set_stb & in_set_has_time;
 
-  // Extract vita time from tuser
+  // Extract vita time from s_axis_data_tuser
   wire [63:0] vita_time_in;
   cvita_hdr_decoder cvita_hdr_decoder_in (
-    .header(s_axis_data_tuser),
-    .pkt_type(), .eob(), .has_time(),
-    .seqnum(), .length(), .payload_length(),
-    .src_sid(), .dst_sid(),
-    .vita_time(vita_time_in));
+    .header         (s_axis_data_tuser),
+    .pkt_type       (),
+    .eob            (),
+    .has_time       (),
+    .seqnum         (),
+    .length         (),
+    .payload_length (),
+    .src_sid        (),
+    .dst_sid        (),
+    .vita_time      (vita_time_in)
+  );
 
   // Track time
   reg header_valid = 1'b1;
@@ -97,6 +133,8 @@ module axi_tag_time #(
     end
   end
 
+  // Create the tags vector by asserting only the tag bit corresponding to the
+  // register being addressed.
   genvar i;
   wire [NUM_TAGS-1:0] tags;
   generate
@@ -110,31 +148,62 @@ module axi_tag_time #(
   wire [NUM_TAGS-1:0] fifo_tags;
   wire fifo_tvalid, fifo_tready;
   wire timed_cmd_fifo_full_n;
-  axi_fifo #(.WIDTH(SR_TWIDTH+NUM_TAGS), .SIZE(CMD_FIFO_SIZE)) axi_fifo (
-    .clk(clk), .reset(reset), .clear(clear),
-    .i_tdata({in_set_time,tags}), .i_tvalid(timed_set_stb), .i_tready(timed_cmd_fifo_full_n),
-    .o_tdata({fifo_set_time,fifo_tags}), .o_tvalid(fifo_tvalid), .o_tready(fifo_tready),
-    .space(), .occupied());
+  axi_fifo #(
+    .WIDTH (SR_TWIDTH+NUM_TAGS),
+    .SIZE  (CMD_FIFO_SIZE)
+  ) axi_fifo (
+    .clk      (clk),
+    .reset    (reset),
+    .clear    (clear),
+    .i_tdata  ({in_set_time,tags}),
+    .i_tvalid (timed_set_stb),
+    .i_tready (timed_cmd_fifo_full_n),
+    .o_tdata  ({fifo_set_time,fifo_tags}),
+    .o_tvalid (fifo_tvalid),
+    .o_tready (fifo_tready),
+    .space    (),
+    .occupied ()
+  );
 
-  // Extract has time from tuser
+  // Extract has_time from m_axis_data_tuser
   wire has_time;
   cvita_hdr_decoder cvita_hdr_decoder_out (
-    .header(m_axis_data_tuser),
-    .pkt_type(), .eob(), .has_time(has_time),
-    .seqnum(), .length(), .payload_length(),
-    .src_sid(), .dst_sid(),
-    .vita_time());
+    .header         (m_axis_data_tuser),
+    .pkt_type       (),
+    .eob            (),
+    .has_time       (has_time),
+    .seqnum         (),
+    .length         (),
+    .payload_length (),
+    .src_sid        (),
+    .dst_sid        (),
+    .vita_time      ()
+  );
 
   assign timed_cmd_fifo_full = ~timed_cmd_fifo_full_n;
-  assign fifo_tready = m_axis_data_tvalid & m_axis_data_tready & fifo_tvalid & has_time & (vita_time_now >= fifo_set_time);
-  assign in_rb_stb = fifo_tready;
+  assign fifo_tready = m_axis_data_tvalid &
+                       m_axis_data_tready &
+                       fifo_tvalid        &
+                       has_time           &
+                       (vita_time_now >= fifo_set_time);
 
-  // Need a single cycle delay to allow vita_time_now to update at the start of a new packet
-  axi_fifo_flop #(.WIDTH(WIDTH+HEADER_WIDTH+1)) axi_fifo_flop (
-    .clk(clk), .reset(reset), .clear(clear),
-    .i_tdata({s_axis_data_tdata,s_axis_data_tuser,s_axis_data_tlast}), .i_tvalid(s_axis_data_tvalid), .i_tready(s_axis_data_tready),
-    .o_tdata({m_axis_data_tdata,m_axis_data_tuser,m_axis_data_tlast}), .o_tvalid(m_axis_data_tvalid), .o_tready(m_axis_data_tready));
+  // Need a single cycle delay to allow vita_time_now to update at the start of
+  // a new packet.
+  axi_fifo_flop #(
+    .WIDTH (WIDTH+HEADER_WIDTH+1)
+  ) axi_fifo_flop (
+    .clk      (clk),
+    .reset    (reset),
+    .clear    (clear),
+    .i_tdata  ({ s_axis_data_tdata, s_axis_data_tuser, s_axis_data_tlast }),
+    .i_tvalid (s_axis_data_tvalid),
+    .i_tready (s_axis_data_tready),
+    .o_tdata  ({ m_axis_data_tdata, m_axis_data_tuser, m_axis_data_tlast }),
+    .o_tvalid (m_axis_data_tvalid),
+    .o_tready (m_axis_data_tready)
+  );
 
-  assign m_axis_data_tag = ((vita_time_now >= fifo_set_time) & fifo_tvalid & has_time) ? fifo_tags : 'd0;
+  assign m_axis_data_tag =
+    ((vita_time_now >= fifo_set_time) & fifo_tvalid & has_time) ? fifo_tags : 'd0;
 
 endmodule
