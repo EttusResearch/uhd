8038727d59d5c359721f0b94698a563083464827
Aaron Rossetto <aaron.rossetto@ni.com>
2021-07-13 07:13:10 -0500

    uhd: Update version, manifest, and changelogs
    Recently, we released UHD v4.1.0.1, which addressed some bugs that found
    their way into the UHD v4.1.0.0 release. While the changes that
    addressed those bugs were made in the master branch, they were
    cherry-picked into the UHD-4.1 branch, and the v4.1.0.1 release was made
    from that branch. However, the version, manifest, and changelogs were
    updated only in the UHD-4.1 branch, not in master. The effect of that is
    that the current master (a.k.a. the absolute latest, perhaps unstable,
    use-at-your-own-risk code) branch's version, manifest, and changelogs
    still reflect the v4.1.0.0 release. Furthermore, the UHD documentation
    refers to the manifest file in the master branch as the way to get (at
    least) the most recent filesystem and FPGA artifacts. Not updating the
    manifest in master renders that documentation inaccurate.
    
    This commit updates those details in master to reflect the latest
    v4.1.0.1 release artifacts, and updates the version of master to
    4.2.0-git.

--- host/cmake/Modules/UHDVersion.cmake
+++ host/cmake/Modules/UHDVersion.cmake
@@ -15,13 +15,15 @@ find_package(Git QUIET)
 #  - Increment API on API changes
 #  - Increment ABI on ABI changes
 #  - Increment patch for bugfixes and docs
+#    (but use 'git' for master to represent 'ahead of the latest stable
+#     release)
 #  - set UHD_VERSION_DEVEL to true for master and development branches
 ########################################################################
 set(UHD_VERSION_MAJOR 4)
-set(UHD_VERSION_API   1)
+set(UHD_VERSION_API   2)
 set(UHD_VERSION_ABI   0)
-set(UHD_VERSION_PATCH 0)
-set(UHD_VERSION_DEVEL FALSE)
+set(UHD_VERSION_PATCH git)
+set(UHD_VERSION_DEVEL TRUE)
 
 ########################################################################
 # If we're on a development branch, we skip the patch version
