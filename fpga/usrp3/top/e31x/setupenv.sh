da336a5a910976d6ecd88b050834af2afe5fc4be
Wade Fife <wade.fife@ettus.com>
2021-08-25 09:24:05 -0500

    fpga: Set default part for sim in setupenv.sh
    This sets the ARCH and PART_ID environment variables so that the
    selected part family is used for simulations by default. This can be
    overridden by changing them in the Makefile for the testbench if a
    testbench requires a specific part family. Prior to this change, the
    default was always ARCH=kintex7, PART_ID=xc7k410t/ffg900/-2, which
    required support for that part to be installed.

--- fpga/usrp3/top/e31x/setupenv.sh
+++ fpga/usrp3/top/e31x/setupenv.sh
@@ -8,4 +8,8 @@ declare -A PRODUCT_ID_MAP
 PRODUCT_ID_MAP["E310_SG1"]="zynq/xc7z020/clg484/-1"
 PRODUCT_ID_MAP["E310_SG3"]="zynq/xc7z020/clg484/-3"
 
+# Set default part for simulation
+export ARCH=zynq
+export PART_ID="xc7z020/clg484/-3"
+
 source $REPO_BASE_PATH/tools/scripts/setupenv_base.sh
