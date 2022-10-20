8c217736df9b12ebab54c7c2f3fd56639d8def16
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-20 19:30:07 +0000

    Adding tarfile member sanitization to extractall()

--- tools/debs/uhd_ubuntu_deb.py
+++ tools/debs/uhd_ubuntu_deb.py
@@ -69,7 +69,26 @@ def main(args):
     if uhd_deb_build_path.exists():
         shutil.rmtree(uhd_deb_build_path)
     with tarfile.open(args.buildpath + "/uhd_{}.orig.tar.xz".format(uhd_version), "r:xz") as uhd_archive:
-        uhd_archive.extractall(path=uhd_deb_build_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(uhd_archive, path=uhd_deb_build_path)
 
     # Copy debian build files to build folder
     shutil.copytree("host/cmake/debian", uhd_deb_build_path / "debian")
