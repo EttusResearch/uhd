3246aec020f0fd248735da617425257c6c5c48dd
Martin Braun <martin.braun@ettus.com>
2023-05-17 09:59:34 +0200

    mpm: x4xx: Check for tear_down()'s existence
    On tear_down() (this is most commonly seen by users when updating the
    FPGA image), we add a check to verify that subcomponents of the X4xx are
    actually initialized, to avoid displaying a non-fatal error message that
    looks like this:
    
    [ERROR] [MPM.RPCServer] Error in update_component while resetting:
        'NoneType' object has no attribute 'tear_down'
    
    The most common/easy way to trigger this error is by loading MPM with
    a bitfile that has an invalid compat number (which would stop MPM from
    initializing the RFDC controls) and then running `uhd_image_loader`
    (which would force a tear_down()). In that circumstance, MPM would try
    to call tear_down() on an uninitialized object.

--- mpm/python/usrp_mpm/periph_manager/x4xx.py
+++ mpm/python/usrp_mpm/periph_manager/x4xx.py
@@ -674,8 +674,10 @@ class x4xx(ZynqComponents, PeriphManagerBase):
         super(x4xx, self).tear_down()
         if self.dio_control is not None:
             self.dio_control.tear_down()
-        self.rfdc.tear_down()
-        self.clk_mgr.tear_down()
+        if self.rfdc:
+            self.rfdc.tear_down()
+        if self.clk_mgr:
+            self.clk_mgr.tear_down()
         # remove x4xx overlay
         active_overlays = self.list_active_overlays()
         self.log.trace("X4xx has active device tree overlays: {}".format(
