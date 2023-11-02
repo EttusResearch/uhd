6a990d9b3297ff2c9f2f91b7ea89494c1098c3f5
Wade Fife <wade.fife@ettus.com>
2023-11-02 13:19:28 -0500

    fpga: x400: Add clock IDs to X440 200 MHz image

--- fpga/usrp3/top/x400/x440_200_rfnoc_image_core.v
+++ fpga/usrp3/top/x400/x440_200_rfnoc_image_core.v
@@ -1093,6 +1093,8 @@ module rfnoc_image_core #(
     .NUM_PORTS           (4),
     .NIPC                (RADIO_NIPC),
     .ITEM_W              (32),
+    .CTRL_CLK_IDX        (1),
+    .TB_CLK_IDX          (4),
     .MTU                 (BLOCK_MTU)
   ) b_radio0_0 (
     .rfnoc_chdr_clk      (rfnoc_chdr_clk),
@@ -1176,6 +1178,8 @@ module rfnoc_image_core #(
     .NUM_PORTS           (4),
     .NIPC                (RADIO_NIPC),
     .ITEM_W              (32),
+    .CTRL_CLK_IDX        (1),
+    .TB_CLK_IDX          (5),
     .MTU                 (BLOCK_MTU)
   ) b_radio1_1 (
     .rfnoc_chdr_clk      (rfnoc_chdr_clk),
