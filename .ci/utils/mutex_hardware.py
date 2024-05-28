27083d0089d9ea305a949ca262064da176671903
Lars Amsel <lars.amsel@ni.com>
2024-05-28 09:20:48 +0200

    CI: Cleanup aquire device via labgrid-client
    Current labgrid-client prints out (unwanted) error during aquire
    when `release --kick` has nothing to release. This change first
    checks whether there is anything to kick. Also, it raises an
    runtime error if any of the calls to labgrid-client fails.

--- .ci/utils/mutex_hardware.py
+++ .ci/utils/mutex_hardware.py
@@ -1,18 +1,19 @@
-# mutex_hardware uses redis get a lock on hardware
-# to prevent other Azure Pipeline agents from use.
-# It also provides helper functions to get devices
-# into a state where it can be used for testing.
+"""mutex_hardware contains helper methods to acquire a lock on a device.
+
+mutex_hardware uses redis to get a lock on hardware to prevent other Azure
+Pipeline agents from use. It also provides helper functions to get devices
+into a state where it can be used for testing.
+"""
 
 import argparse
-import labgrid
 import os
 import pathlib
 import shlex
-import socket
 import subprocess
 import sys
 import time
 
+import labgrid
 from fabric import Connection
 from httpd import HTTPServer
 from pottery import Redlock
@@ -21,34 +22,89 @@ from tftp import TFTPServer
 
 bitfile_name = "usrp_{}_fpga_{}.bit"
 
-def jtag_x3xx(dev_type, dev_model, jtag_server, jtag_serial, fpga_folder, fpga, redis_server, vivado_dir):
+
+def aquire(labgrid_device_yaml):
+    """Acquire a device defined in labgrid_device_yaml.
+
+    This function first checks whether there is a lock on the device at all using the
+    show command. The lock will only be released if the device is acquired at all
+    (otherwise the log will contain an error printout that the device is not acquired
+    which cannot be suppressed).
+    If it is ensured there is no device lock on the device, the device is acquired.
+    The function will raise a RuntimeError if any of the steps fail.
+
+    Args: labgrid_device_yaml (str): Path to labgrid device yaml file.
+    Raises: RuntimeError: If device cannot be acquired or previous lock cannot be released.
+    Returns: None
+    """
+    run_result = subprocess.run(
+        args=shlex.split(f'labgrid-client -c {labgrid_device_yaml} show | grep "acquired: None"'),
+        check=False,
+    )
+
+    if run_result.returncode > 0:
+        run_result = subprocess.run(
+            args=shlex.split(f"labgrid-client -c {labgrid_device_yaml} release --kick"), check=False
+        )
+        if not run_result.returncode:
+            raise RuntimeError(
+                f"Failed to force release device {labgrid_device_yaml} (return code {run_result.returncode})"
+            )
+
+    run_result = subprocess.run(
+        args=shlex.split(f"labgrid-client -c {labgrid_device_yaml} acquire"), check=None
+    )
+    if not run_result.returncode:
+        raise RuntimeError(
+            f"Failed to aquire device {labgrid_device_yaml} (return code {run_result.returncode})"
+        )
+
+
+def jtag_x3xx(
+    dev_type, dev_model, jtag_server, jtag_serial, fpga_folder, fpga, redis_server, vivado_dir
+):
+    """Download the bitfile to the FPGA using jtag interface."""
     if dev_model not in ["x300", "x310"]:
-        raise RuntimeError(f'{dev_type} not supported with jtag_x3xx')
+        raise RuntimeError(f"{dev_type} not supported with jtag_x3xx")
     remote_working_dir = "pipeline_fpga"
     vivado_lab_path = os.path.join(vivado_dir, "bin/vivado_lab")
     vivado_program_jtag = "{} -mode batch -source {}/viv_hardware_utils.tcl -nolog -nojournal -tclargs program".format(
-        vivado_lab_path, remote_working_dir)
+        vivado_lab_path, remote_working_dir
+    )
     print("Waiting on jtag mutex for {}".format(jtag_server), flush=True)
-    with Redlock(key="hw_jtag_{}".format(jtag_server),
-                 masters=redis_server, auto_release_time=1000 * 60 * 5):
+    with Redlock(
+        key="hw_jtag_{}".format(jtag_server), masters=redis_server, auto_release_time=1000 * 60 * 5
+    ):
         print("Got jtag mutex for {}".format(jtag_server), flush=True)
         with Connection(host=jtag_server) as jtag_host:
             jtag_host.run("mkdir -p " + remote_working_dir)
             jtag_host.run("rm -rf {}/*".format(remote_working_dir))
             jtag_host.put(
-                os.path.join(pathlib.Path(
-                    __file__).parent.absolute(), "jtag/viv_hardware_utils.tcl"),
-                remote=remote_working_dir)
+                os.path.join(
+                    pathlib.Path(__file__).parent.absolute(), "jtag/viv_hardware_utils.tcl"
+                ),
+                remote=remote_working_dir,
+            )
             fpga_path = os.path.join(fpga_folder, bitfile_name.format(dev_model, fpga))
             jtag_host.put(fpga_path, remote=remote_working_dir)
-            jtag_host.run(vivado_program_jtag + " " +
-                          os.path.join(remote_working_dir, os.path.basename(fpga_path)) +
-                          " " + jtag_serial)
+            jtag_host.run(
+                vivado_program_jtag
+                + " "
+                + os.path.join(remote_working_dir, os.path.basename(fpga_path))
+                + " "
+                + jtag_serial
+            )
         print("Waiting 15 seconds for device to come back up and for Vivado to close", flush=True)
         time.sleep(15)
 
+
 def set_sfp_addrs(mgmt_addr, sfp_addrs):
-    with Connection(host=mgmt_addr,user='root',connect_kwargs={"password":"", "timeout":120, "banner_timeout":120, "auth_timeout":120}) as dut:
+    """Configure IP addresses of the SFP adapters."""
+    with Connection(
+        host=mgmt_addr,
+        user="root",
+        connect_kwargs={"password": "", "timeout": 120, "banner_timeout": 120, "auth_timeout": 120},
+    ) as dut:
         for idx, sfp_addr in enumerate(sfp_addrs):
             dut.run(f"ip link set sfp{idx} down")
             dut.run(f"ip addr flush dev sfp{idx}")
@@ -56,14 +112,16 @@ def set_sfp_addrs(mgmt_addr, sfp_addrs):
             dut.run(f"ip link set sfp{idx} up")
     time.sleep(30)
 
+
 def flash_sdimage_sdmux(dev_model, sdimage_path, labgrid_device_yaml, mgmt_addr, sfp_addrs):
-    """ This method uses an sdmux (https://linux-automation.com/en/products/usb-sd-mux.html)
+    """Flash image using sdmux.
+
+    This method uses an sdmux (https://linux-automation.com/en/products/usb-sd-mux.html)
     to reimage the sd card.
     """
     if dev_model not in ["n300", "n310", "n320", "n321"]:
-        raise RuntimeError(f'{dev_model} not supported with sdimage_sdmux')
-    subprocess.run(shlex.split(f"labgrid-client -c {labgrid_device_yaml} release --kick"))
-    subprocess.run(shlex.split(f"labgrid-client -c {labgrid_device_yaml} acquire"))
+        raise RuntimeError(f"{dev_model} not supported with sdimage_sdmux")
+    aquire(labgrid_device_yaml)
     env = labgrid.Environment(labgrid_device_yaml)
     target = env.get_target()
 
@@ -75,7 +133,7 @@ def flash_sdimage_sdmux(dev_model, sdimage_path, labgrid_device_yaml, mgmt_addr,
 
     print("Switching SDMux to Host", flush=True)
     sdmux = target.get_driver(labgrid.driver.USBSDMuxDriver)
-    sdmux.set_mode('host')
+    sdmux.set_mode("host")
 
     print(f"Writing SDMux using {sdimage_path}", flush=True)
     massstore = target.get_driver(labgrid.driver.USBStorageDriver)
@@ -87,7 +145,7 @@ def flash_sdimage_sdmux(dev_model, sdimage_path, labgrid_device_yaml, mgmt_addr,
     time.sleep(30)
 
     print("Switching SDMux to DUT", flush=True)
-    sdmux.set_mode('dut')
+    sdmux.set_mode("dut")
     time.sleep(30)
 
     print("Powering on DUT", flush=True)
@@ -105,27 +163,31 @@ def flash_sdimage_sdmux(dev_model, sdimage_path, labgrid_device_yaml, mgmt_addr,
     time.sleep(120)
     cp_linux.expect("login:", timeout=5)
     known_hosts_path = os.path.expanduser("~/.ssh/known_hosts")
-    subprocess.run(shlex.split(f"ssh-keygen -f \"{known_hosts_path}\" -R \"{mgmt_addr}\""))
+    subprocess.run(shlex.split(f'ssh-keygen -f "{known_hosts_path}" -R "{mgmt_addr}"'))
 
     if sfp_addrs:
         set_sfp_addrs(mgmt_addr, sfp_addrs)
 
     subprocess.run(shlex.split(f"labgrid-client -c {labgrid_device_yaml} release"))
 
-def flash_sdimage_tftp(dev_model, sdimage_path, initramfs_path, labgrid_device_yaml, sfp_addrs, redis_server):
-    """ This method uses tftp to boot the device into a small Linux envionment to
+
+def flash_sdimage_tftp(
+    dev_model, sdimage_path, initramfs_path, labgrid_device_yaml, sfp_addrs, redis_server
+):
+    """Flash image using tftp.
+
+    This method uses tftp to boot the device into a small Linux envionment to
     write to the device's sd card. This method is used on the E320 since it has
     a hardware incompatibility with sdmuxes.
     """
     if dev_model not in ["e320"]:
-        raise RuntimeError(f'{dev_model} not supported with sdimage_tftp')
+        raise RuntimeError(f"{dev_model} not supported with sdimage_tftp")
 
     if dev_model == "e320":
-        dev_ram_address = '0x20000000'
-        dev_bootm_config = 'conf@zynq-ni-${mboard}.dtb'
+        dev_ram_address = "0x20000000"
+        dev_bootm_config = "conf@zynq-ni-${mboard}.dtb"
 
-    subprocess.run(shlex.split(f"labgrid-client -c {labgrid_device_yaml} release --kick"))
-    subprocess.run(shlex.split(f"labgrid-client -c {labgrid_device_yaml} acquire"))
+    aquire(labgrid_device_yaml)
     env = labgrid.Environment(labgrid_device_yaml)
     target = env.get_target()
 
@@ -159,7 +221,9 @@ def flash_sdimage_tftp(dev_model, sdimage_path, initramfs_path, labgrid_device_y
     time.sleep(10)
     cp_linux.write(f"setenv autoload no; dhcp;\n".encode())
     cp_linux.expect("DHCP client bound to address")
-    expect_index, expect_before, expect_match , expect_after = cp_linux.expect(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b")
+    expect_index, expect_before, expect_match, expect_after = cp_linux.expect(
+        r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
+    )
     mgmt_addr = expect_match[0].decode()
     print(f"Dev got IP Address {mgmt_addr}")
 
@@ -169,7 +233,9 @@ def flash_sdimage_tftp(dev_model, sdimage_path, initramfs_path, labgrid_device_y
         cp_linux.write(f"setenv tftpdstp {server.port}\n".encode())
         cp_linux.expect("uboot>")
         print("TFTPing initramfs image", flush=True)
-        cp_linux.write(f"tftpboot {dev_ram_address} {server.ip}:{os.path.basename(initramfs_path)}\n".encode())
+        cp_linux.write(
+            f"tftpboot {dev_ram_address} {server.ip}:{os.path.basename(initramfs_path)}\n".encode()
+        )
         cp_linux.expect("uboot>", timeout=120)
         print("Booting into initramfs", flush=True)
         cp_linux.write(f"bootm {dev_ram_address}#{dev_bootm_config}\n".encode())
@@ -183,7 +249,9 @@ def flash_sdimage_tftp(dev_model, sdimage_path, initramfs_path, labgrid_device_y
     with HTTPServer(os.path.dirname(sdimage_path), mgmt_addr) as server:
         print(f"Writing SD Card using {sdimage_path}", flush=True)
         print("Running bmaptool... This will take awhile", flush=True)
-        cp_linux.write(f"bmaptool copy --nobmap {server.get_url(os.path.basename(sdimage_path))} /dev/mmcblk0\n".encode())
+        cp_linux.write(
+            f"bmaptool copy --nobmap {server.get_url(os.path.basename(sdimage_path))} /dev/mmcblk0\n".encode()
+        )
         cp_linux.expect("mender:~#", timeout=1800)
         cp_linux.write("echo bmaptool exit code: $?\n".encode())
         cp_linux.expect("bmaptool exit code: 0", timeout=10)
@@ -195,59 +263,94 @@ def flash_sdimage_tftp(dev_model, sdimage_path, initramfs_path, labgrid_device_y
     time.sleep(120)
     cp_linux.expect("login:", timeout=30)
     known_hosts_path = os.path.expanduser("~/.ssh/known_hosts")
-    subprocess.run(shlex.split(f"ssh-keygen -f \"{known_hosts_path}\" -R \"{mgmt_addr}\""))
+    subprocess.run(
+        args=shlex.split(f'ssh-keygen -f "{known_hosts_path}" -R "{mgmt_addr}"'), check=False
+    )
 
     if sfp_addrs:
         set_sfp_addrs(mgmt_addr, sfp_addrs)
 
-    subprocess.run(shlex.split(f"labgrid-client -c {labgrid_device_yaml} release"))
+    subprocess.run(
+        args=shlex.split(f"labgrid-client -c {labgrid_device_yaml} release"), check=False
+    )
     return mgmt_addr
 
+
 def main(args):
-    redis_server = {Redis.from_url(
-        "redis://{}:6379/0".format(args.redis_server))}
+    """Main entry point for mutex_hardware."""
+    redis_server = {Redis.from_url("redis://{}:6379/0".format(args.redis_server))}
     print("Waiting to acquire mutex for {}".format(args.dut_name), flush=True)
-    with Redlock(key=args.dut_name, masters=redis_server, auto_release_time=1000 * 60 * args.dut_timeout):
+    with Redlock(
+        key=args.dut_name, masters=redis_server, auto_release_time=1000 * 60 * args.dut_timeout
+    ):
         print("Got mutex for {}".format(args.dut_name), flush=True)
 
         if args.sdimage_sdmux:
-            dev_type, dev_model, sdimage_path, labgrid_device_yaml, mgmt_addr = args.sdimage_sdmux.split(',')
+            dev_type, dev_model, sdimage_path, labgrid_device_yaml, mgmt_addr = (
+                args.sdimage_sdmux.split(",")
+            )
             if args.sfp_addrs:
-                sfp_addrs = args.sfp_addrs.split(',')
+                sfp_addrs = args.sfp_addrs.split(",")
             else:
                 sfp_addrs = None
             flash_sdimage_sdmux(dev_model, sdimage_path, labgrid_device_yaml, mgmt_addr, sfp_addrs)
 
         if args.sdimage_tftp:
-            dev_type, dev_model, sdimage_path, initramfs_path, labgrid_device_yaml = args.sdimage_tftp.split(',')
+            dev_type, dev_model, sdimage_path, initramfs_path, labgrid_device_yaml = (
+                args.sdimage_tftp.split(",")
+            )
             if args.sfp_addrs:
-                sfp_addrs = args.sfp_addrs.split(',')
+                sfp_addrs = args.sfp_addrs.split(",")
             else:
                 sfp_addrs = None
-            mgmt_addr = flash_sdimage_tftp(dev_model, sdimage_path, initramfs_path, labgrid_device_yaml, sfp_addrs, redis_server)
+            mgmt_addr = flash_sdimage_tftp(
+                dev_model,
+                sdimage_path,
+                initramfs_path,
+                labgrid_device_yaml,
+                sfp_addrs,
+                redis_server,
+            )
 
         if args.fpgas:
             working_dir = os.getcwd()
             return_code = 0
-            for fpga in args.fpgas.split(','):
+            for fpga in args.fpgas.split(","):
                 os.mkdir(os.path.join(working_dir, fpga))
                 os.chdir(os.path.join(working_dir, fpga))
 
                 if args.jtag_x3xx:
-                    dev_type, dev_model, jtag_server, jtag_serial, fpga_folder, vivado_dir = args.jtag_x3xx.split(',')
-                    jtag_x3xx(dev_type, dev_model, jtag_server, jtag_serial, fpga_folder, fpga, redis_server, vivado_dir)
+                    dev_type, dev_model, jtag_server, jtag_serial, fpga_folder, vivado_dir = (
+                        args.jtag_x3xx.split(",")
+                    )
+                    jtag_x3xx(
+                        dev_type,
+                        dev_model,
+                        jtag_server,
+                        jtag_serial,
+                        fpga_folder,
+                        fpga,
+                        redis_server,
+                        vivado_dir,
+                    )
 
                 if dev_type and dev_type in ["n3xx", "e3xx"]:
-                    subprocess.run(shlex.split(f"uhd_image_loader --args=mgmt_addr={mgmt_addr},type={dev_type},fpga={fpga}"))
+                    subprocess.run(
+                        shlex.split(
+                            f"uhd_image_loader --args=mgmt_addr={mgmt_addr},type={dev_type},fpga={fpga}"
+                        )
+                    )
                     if sfp_addrs:
                         set_sfp_addrs(mgmt_addr, sfp_addrs)
 
                 for command in args.test_commands:
                     if args.working_dir:
-                        result = subprocess.run(shlex.split(command.format(fpga=fpga)), cwd=args.working_dir)
+                        result = subprocess.run(
+                            shlex.split(command.format(fpga=fpga)), cwd=args.working_dir
+                        )
                     else:
                         result = subprocess.run(shlex.split(command.format(fpga=fpga)))
-                    if(return_code == 0):
+                    if return_code == 0:
                         return_code = result.returncode
             sys.exit(return_code)
         else:
@@ -256,9 +359,10 @@ def main(args):
                     result = subprocess.run(shlex.split(command), cwd=args.working_dir)
                 else:
                     result = subprocess.run(shlex.split(command))
-                if(result.returncode != 0):
+                if result.returncode != 0:
                     sys.exit(result.returncode)
             sys.exit(0)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -267,28 +371,30 @@ if __name__ == "__main__":
     # Vivado on jtag_server. It uses SSH to control jtag_server.
     # Provide fpga_path as a local path and it will be copied
     # to jtag_server.
-    group.add_argument("--jtag_x3xx", type=str,
-                        help="dev_type,dev_model,user@jtag_server,jtag_serial,fpga_folder")
-    group.add_argument("--sdimage_sdmux", type=str,
-                        help="dev_type,dev_model,sdimg_path,labgrid_device_yaml,mgmt_addr")
-    group.add_argument("--sdimage_tftp", type=str,
-                        help="dev_type,dev_model,sdimg_path,initramfs_path,labgrid_device_yaml")
-    parser.add_argument("--sfp_addrs", type=str,
-                        help="sfp0ip,sfp1ip,...")
-    parser.add_argument("--fpgas", type=str,
-                        help="Comma delimited list of FPGAs to test")
-    parser.add_argument("--dut_timeout", type=int, default=60,
-                        help="Dut mutex timeout in minutes")
-    parser.add_argument("--working_dir", type=str,
-                        help="Change working directory for commands to be run")
-    parser.add_argument("redis_server", type=str,
-                        help="Redis server for mutex")
-    parser.add_argument("dut_name", type=str,
-                        help="Unique identifier for device under test")
+    group.add_argument(
+        "--jtag_x3xx", type=str, help="dev_type,dev_model,user@jtag_server,jtag_serial,fpga_folder"
+    )
+    group.add_argument(
+        "--sdimage_sdmux",
+        type=str,
+        help="dev_type,dev_model,sdimg_path,labgrid_device_yaml,mgmt_addr",
+    )
+    group.add_argument(
+        "--sdimage_tftp",
+        type=str,
+        help="dev_type,dev_model,sdimg_path,initramfs_path,labgrid_device_yaml",
+    )
+    parser.add_argument("--sfp_addrs", type=str, help="sfp0ip,sfp1ip,...")
+    parser.add_argument("--fpgas", type=str, help="Comma delimited list of FPGAs to test")
+    parser.add_argument("--dut_timeout", type=int, default=60, help="Dut mutex timeout in minutes")
+    parser.add_argument(
+        "--working_dir", type=str, help="Change working directory for commands to be run"
+    )
+    parser.add_argument("redis_server", type=str, help="Redis server for mutex")
+    parser.add_argument("dut_name", type=str, help="Unique identifier for device under test")
     # test_commands allows for any number of shell commands
     # to execute. Call into mutex_hardware with an unlimited
     # number of commands in string format as the last positional arguments.
-    parser.add_argument("test_commands", type=str,
-                        nargs="+", help="Commands to run")
+    parser.add_argument("test_commands", type=str, nargs="+", help="Commands to run")
     args = parser.parse_args()
     main(args)
