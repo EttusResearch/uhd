09f125f6313b7bd17b87e1e536829506ae607956
Grant Meyerhoff <grant.meyerhoff@ni.com>
2024-07-22 16:24:06 -0500

    python: Add windows dll import logic
    In python3.8, a change was made to the DLL search path, which requires using os.add_dll_directory to add a non-default path.

--- host/python/copy_python_module.cmake
+++ host/python/copy_python_module.cmake
@@ -1,5 +1,5 @@
 #
-# Copyright 2017 Ettus Research, a National Instruments Company
+# Copyright 2024 Ettus Research, a National Instruments Company
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
@@ -7,5 +7,5 @@
 ########################################################################
 set(BINARY_DIR "" CACHE STRING "")
 set(SOURCE_DIR "" CACHE STRING "")
-file(COPY "${SOURCE_DIR}/usrp_mpm/" DESTINATION ${BINARY_DIR}/usrp_mpm
-    FILES_MATCHING PATTERN *.py)
+file(COPY "${SOURCE_DIR}/uhd/" DESTINATION "${BINARY_DIR}/uhd"
+    FILES_MATCHING PATTERN "*.py" PATTERN "*.mako")
