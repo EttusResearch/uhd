87c4ff673a9e0d095d568f12e85f02d688aba727
Martin Braun <martin.braun@ettus.com>
2024-11-11 15:06:43 +0100

    lib: dsp: Refactor dsp_core_utils
    This refactors get_freq_and_freq_word(). It had the following issues:
    
    - It used a static const for the scaling factor, which depends on the
      word width. Previously, we (incorrectly) used different word widths
      for B200 and RFNoC devices, but because of the static const nature of
      the calculated scaling factor, this could cause incorrect frequency
      words being calculated when running both a B2xx and RFNoC device in
      the same session.
      (Note that this bug can no longer be triggered with the bug fix to
      the word widths, but this cleans up this potential pitfall regardless)
    - In general, the word width shouldn't be a runtime parameter (it
      depends on hardware), and thus was made a template parameter. This
      removes a runtime call to `std::pow()`, and uses a `constexpr`
      instead. This function is rarely cause for performance issues, so this
      change is mostly cosmetic in practice, but it's good practice
      nevertheless.
    - Due to templatization, the code was moved from the .cpp file to its
      header (the .cpp file is no longer required and was removed).
    - While we're modifying this file, we remove all unnecessary
      multiplications/divisions and use normalized values where possible.

--- host/lib/include/uhdlib/usrp/cores/dsp_core_utils.hpp
+++ host/lib/include/uhdlib/usrp/cores/dsp_core_utils.hpp
@@ -7,21 +7,66 @@
 
 #pragma once
 
+#include <uhd/exception.hpp>
+#include <uhd/utils/math.hpp>
+#include <uhdlib/usrp/cores/dsp_core_utils.hpp>
+#include <cmath>
+#include <limits>
 #include <tuple>
 
 /*! For a requested frequency, sampling rate, and frequency word width (in
  *  number of bits), return the correct frequency word (to set the CORDIC or
  *  DDS) and the actual frequency.
  */
+template <int word_width = 32>
 void get_freq_and_freq_word(const double requested_freq,
     const double tick_rate,
     double& actual_freq,
-    int32_t& freq_word,
-    int word_width = 32);
+    int32_t& freq_word)
+{
+    constexpr int32_t MAX_FREQ_WORD = std::numeric_limits<int32_t>::max();
+    constexpr int32_t MIN_FREQ_WORD = std::numeric_limits<int32_t>::min();
+    constexpr double scale_factor   = static_cast<double>(uint64_t(1) << word_width);
+    // Frequency normalized by sampling rate, and wrapped to [-0.5, 0.5).
+    const double freq_norm_scaled =
+        uhd::math::wrap_frequency(requested_freq / tick_rate, 1.0) * scale_factor;
+
+    // confirm that the target frequency is within range of the CORDIC
+    UHD_ASSERT_THROW(std::abs(freq_norm_scaled) - 1 <= MAX_FREQ_WORD);
+
+    /* Now calculate the frequency word. It is possible for this calculation
+     * to cause an overflow. As the requested DSP frequency approaches the
+     * master clock rate, that ratio multiplied by the scaling factor (2^32)
+     * will generally overflow within the last few kHz of tunable range.
+     * Thus, we check to see if the operation will overflow before doing it,
+     * and if it will, we set it to the integer min or max of this system.
+     */
+    if (freq_norm_scaled >= MAX_FREQ_WORD) {
+        /* Operation would have caused a positive overflow of int32. */
+        freq_word = MAX_FREQ_WORD;
+
+    } else if (freq_norm_scaled <= MIN_FREQ_WORD) {
+        /* Operation would have caused a negative overflow of int32. */
+        freq_word = MIN_FREQ_WORD;
+
+    } else {
+        /* The operation is safe. Perform normally. */
+        freq_word = int32_t(std::lround(freq_norm_scaled));
+    }
+
+    actual_freq = (double(freq_word) / scale_factor) * tick_rate;
+}
 
 /*! For a requested frequency, sampling rate, and frequency word width (in
  *  number of bits), return the correct frequency word (to set the CORDIC or
  *  DDS) and the actual frequency.
  */
+template <int word_width = 32>
 std::tuple<double, int> get_freq_and_freq_word(
-    const double requested_freq, const double tick_rate, int word_width = 32);
+    const double requested_freq, const double tick_rate)
+{
+    double actual_freq;
+    int32_t freq_word;
+    get_freq_and_freq_word<word_width>(requested_freq, tick_rate, actual_freq, freq_word);
+    return {actual_freq, freq_word};
+}
