EttusResearch/uhd

commit 699870d7dec67b43c08e55fcc1d4e159a337c49a

RFNoC: Fix DSP frequency accuracy
The host code was calculating and programming a 32-bit value for the DSP
frequency, but the DDS modules in the FPGA only use the upper 24-bits.
This led to inaccurate frequency values being returned.  This change
corrects the resolution of the value on the host side so an accurate
value is returned.

Signed-off-by: michael-west <michael.west@ettus.com>

host/lib/rfnoc/ddc_block_control.cpp
@@ -541,10 +541,15 @@ private:
     double _set_freq(
         const double requested_freq, const double dds_rate, const size_t chan)
     {
+        static int freq_word_width = 24;
         double actual_freq;
         int32_t freq_word;
         std::tie(actual_freq, freq_word) =
-            get_freq_and_freq_word(requested_freq, dds_rate);
+            get_freq_and_freq_word(requested_freq, dds_rate, freq_word_width);
+
+        // Only the upper 24 bits of the SR_FREQ_ADDR register are used, so shift the word
+        freq_word <<= (32 - freq_word_width);
+
         _ddc_reg_iface.poke32(
             SR_FREQ_ADDR, uint32_t(freq_word), chan, get_command_time(chan));
         return actual_freq;

host/lib/rfnoc/duc_block_control.cpp
@@ -525,10 +525,15 @@ private:
     double _set_freq(
         const double requested_freq, const double dds_rate, const size_t chan)
     {
+        static int freq_word_width = 24;
         double actual_freq;
         int32_t freq_word;
         std::tie(actual_freq, freq_word) =
-            get_freq_and_freq_word(requested_freq, dds_rate);
+            get_freq_and_freq_word(requested_freq, dds_rate, freq_word_width);
+
+        // Only the upper 24 bits of the SR_FREQ_ADDR register are used, so shift the word
+        freq_word <<= (32 - freq_word_width);
+
         _duc_reg_iface.poke32(
             SR_FREQ_ADDR, uint32_t(freq_word), chan, get_command_time(chan));
         return actual_freq;

host/lib/include/uhdlib/usrp/cores/dsp_core_utils.hpp
@@ -9,16 +9,19 @@
 
 #include <tuple>
 
-/*! For a requested frequency and sampling rate, return the
- *  correct frequency word (to set the CORDIC) and the actual frequency.
+/*! For a requested frequency, sampling rate, and frequency word width (in
+ *  number of bits), return the correct frequency word (to set the CORDIC or
+ *  DDS) and the actual frequency.
  */
 void get_freq_and_freq_word(const double requested_freq,
     const double tick_rate,
     double& actual_freq,
-    int32_t& freq_word);
+    int32_t& freq_word,
+    int word_width = 32);
 
-/*! For a requested frequency and sampling rate, return the
- *  correct frequency word (to set the CORDIC) and the actual frequency.
+/*! For a requested frequency, sampling rate, and frequency word width (in
+ *  number of bits), return the correct frequency word (to set the CORDIC or
+ *  DDS) and the actual frequency.
  */
 std::tuple<double, int> get_freq_and_freq_word(
-    const double requested_freq, const double tick_rate);
+    const double requested_freq, const double tick_rate, int word_width = 32);

host/lib/usrp/cores/dsp_core_utils.cpp
@@ -17,7 +17,8 @@ static const int32_t MIN_FREQ_WORD = std::numeric_limits<int32_t>::min();
 void get_freq_and_freq_word(const double requested_freq,
     const double tick_rate,
     double& actual_freq,
-    int32_t& freq_word)
+    int32_t& freq_word,
+    int word_width)
 {
     const double freq = uhd::math::wrap_frequency(requested_freq, tick_rate);
 
@@ -33,7 +34,7 @@ void get_freq_and_freq_word(const double requested_freq,
      */
     freq_word = 0;
 
-    static const double scale_factor = std::pow(2.0, 32);
+    static const double scale_factor = std::pow(2.0, word_width);
     if ((freq / tick_rate) >= (MAX_FREQ_WORD / scale_factor)) {
         /* Operation would have caused a positive overflow of int32. */
         freq_word = MAX_FREQ_WORD;
@@ -51,10 +52,10 @@ void get_freq_and_freq_word(const double requested_freq,
 }
 
 std::tuple<double, int> get_freq_and_freq_word(
-    const double requested_freq, const double tick_rate)
+    const double requested_freq, const double tick_rate, int word_width)
 {
     double actual_freq;
     int32_t freq_word;
-    get_freq_and_freq_word(requested_freq, tick_rate, actual_freq, freq_word);
+    get_freq_and_freq_word(requested_freq, tick_rate, actual_freq, freq_word, word_width);
     return std::make_tuple(actual_freq, freq_word);
 }

commit 635ad362ae60f82b3131970d6f8171505dce6ac7

lib: rfnoc ddc/duc: Fix fractional frequency offset
The RFNoC DDC/DUC block controllers had a bug, in which they would only
program the upper 24 bits of the 32-bit phase increment value for the
digital frequency shifter.

The DDC/DUC use a 24-bit DDS + multiplier to shift frequency digitally,
but the phase increment input to the DDS is a 32-bit value, which we
were (incorrectly) quantizing to a 24-bit value. This meant we were
artificially limiting the digital frequency accuracy, e.g., for a 200
MHz sample clock, to 200MHz / 2^{24} == (approx.) 12 Hz, which in turn
meant there was an unnecessary frequency error of up to 6 Hz (for lower
clock rates, the error would be smaller but still unnecessarily high).

This patch fixes the width of the phase increment value and allows for
sub-Hz accuracy (once again).

This bug was introduced in 699870d (first released with UHD 4.2).

host/lib/rfnoc/ddc_block_control.cpp
@@ -653,14 +653,10 @@ private:
     double _set_freq(
         const double requested_freq, const double dds_rate, const size_t chan)
     {
-        static int freq_word_width = 24;
         double actual_freq;
         int32_t freq_word;
         std::tie(actual_freq, freq_word) =
-            get_freq_and_freq_word(requested_freq, dds_rate, freq_word_width);
-
-        // Only the upper 24 bits of the SR_FREQ_ADDR register are used, so shift the word
-        freq_word <<= (32 - freq_word_width);
+            get_freq_and_freq_word(requested_freq, dds_rate);
 
         _ddc_reg_iface.poke32(
             SR_FREQ_ADDR, uint32_t(freq_word), chan, get_command_time(chan));

host/lib/rfnoc/duc_block_control.cpp
@@ -645,14 +645,10 @@ private:
     double _set_freq(
         const double requested_freq, const double dds_rate, const size_t chan)
     {
-        static int freq_word_width = 24;
         double actual_freq;
         int32_t freq_word;
         std::tie(actual_freq, freq_word) =
-            get_freq_and_freq_word(requested_freq, dds_rate, freq_word_width);
-
-        // Only the upper 24 bits of the SR_FREQ_ADDR register are used, so shift the word
-        freq_word <<= (32 - freq_word_width);
+            get_freq_and_freq_word(requested_freq, dds_rate);
 
         _duc_reg_iface.poke32(
             SR_FREQ_ADDR, uint32_t(freq_word), chan, get_command_time(chan));