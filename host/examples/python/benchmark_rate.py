e40121441eaf8f92adb8a92e3a1c8e1ab28c39fe
Grant Meyerhoff <grant.meyerhoff@ni.com>
2025-01-03 14:04:44 -0600

    host: update python formatting

--- host/examples/python/benchmark_rate.py
+++ host/examples/python/benchmark_rate.py
@@ -5,25 +5,24 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-"""
-Benchmark rate using Python API
-"""
+"""Benchmark rate using Python API."""
 
 import argparse
-from datetime import datetime, timedelta
-import sys
-import time
-import threading
 import logging
+import sys
+import threading
+import time
+from datetime import datetime, timedelta
+
 import numpy as np
 import uhd
-
 
 CLOCK_TIMEOUT = 1000  # 1000mS timeout for external clock locking
 INIT_DELAY = 0.05  # 50mS initial delay before transmit
 
+
 def parse_args():
-    """Parse the command line arguments"""
+    """Parse the command line arguments."""
     description = """UHD Benchmark Rate (Python API)
 
         Utility to stress test a USRP device.
@@ -32,52 +31,74 @@ def parse_args():
         Specify --tx_rate for a transmit-only test.
         Specify both options for a full-duplex test.
         """
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
-                                     description=description)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter, description=description
+    )
     parser.add_argument("-a", "--args", default="", type=str, help="single uhd device address args")
-    parser.add_argument("-d", "--duration", default=10.0, type=float,
-                        help="duration for the test in seconds")
+    parser.add_argument(
+        "-d", "--duration", default=10.0, type=float, help="duration for the test in seconds"
+    )
     parser.add_argument("--rx_subdev", type=str, help="specify the device subdev for RX")
     parser.add_argument("--tx_subdev", type=str, help="specify the device subdev for TX")
     parser.add_argument("--rx_rate", type=float, help="specify to perform a RX rate test (sps)")
     parser.add_argument("--tx_rate", type=float, help="specify to perform a TX rate test (sps)")
-    parser.add_argument("--rx_otw", type=str, default="sc16",
-                        help="specify the over-the-wire sample mode for RX")
-    parser.add_argument("--tx_otw", type=str, default="sc16",
-                        help="specify the over-the-wire sample mode for TX")
-    parser.add_argument("--rx_cpu", type=str, default="fc32",
-                        help="specify the host/cpu sample mode for RX")
-    parser.add_argument("--tx_cpu", type=str, default="fc32",
-                        help="specify the host/cpu sample mode for TX")
-    parser.add_argument("--rx_stream_args",
-                        help="stream args for RX streamer", default="")
-    parser.add_argument("--tx_stream_args", help="stream args for TX streamer",
-                        default="")
-    parser.add_argument("--ref", type=str,
-                        help="clock reference (internal, external, mimo, gpsdo)")
+    parser.add_argument(
+        "--rx_otw", type=str, default="sc16", help="specify the over-the-wire sample mode for RX"
+    )
+    parser.add_argument(
+        "--tx_otw", type=str, default="sc16", help="specify the over-the-wire sample mode for TX"
+    )
+    parser.add_argument(
+        "--rx_cpu", type=str, default="fc32", help="specify the host/cpu sample mode for RX"
+    )
+    parser.add_argument(
+        "--tx_cpu", type=str, default="fc32", help="specify the host/cpu sample mode for TX"
+    )
+    parser.add_argument("--rx_stream_args", help="stream args for RX streamer", default="")
+    parser.add_argument("--tx_stream_args", help="stream args for TX streamer", default="")
+    parser.add_argument("--ref", type=str, help="clock reference (internal, external, mimo, gpsdo)")
     parser.add_argument("--pps", type=str, help="PPS source (internal, external, mimo, gpsdo)")
-    parser.add_argument("--random", action="store_true", default=False,
-                        help="Run with random values of samples in send() and recv() to stress-test"
-                             " the I/O.")
-    parser.add_argument("-c", "--channels", default=[0], nargs="+", type=int,
-                        help="which channel(s) to use (specify \"0\", \"1\", \"0 1\", etc)")
-    parser.add_argument("--rx_channels", nargs="+", type=int,
-                        help="which RX channel(s) to use (specify \"0\", \"1\", \"0 1\", etc)")
-    parser.add_argument("--tx_channels", nargs="+", type=int,
-                        help="which TX channel(s) to use (specify \"0\", \"1\", \"0 1\", etc)")
+    parser.add_argument(
+        "--random",
+        action="store_true",
+        default=False,
+        help="Run with random values of samples in send() and recv() to stress-test" " the I/O.",
+    )
+    parser.add_argument(
+        "-c",
+        "--channels",
+        default=[0],
+        nargs="+",
+        type=int,
+        help='which channel(s) to use (specify "0", "1", "0 1", etc)',
+    )
+    parser.add_argument(
+        "--rx_channels",
+        nargs="+",
+        type=int,
+        help='which RX channel(s) to use (specify "0", "1", "0 1", etc)',
+    )
+    parser.add_argument(
+        "--tx_channels",
+        nargs="+",
+        type=int,
+        help='which TX channel(s) to use (specify "0", "1", "0 1", etc)',
+    )
     return parser.parse_args()
 
 
 class LogFormatter(logging.Formatter):
-    """Log formatter which prints the timestamp with fractional seconds"""
+    """Log formatter which prints the timestamp with fractional seconds."""
+
     @staticmethod
     def pp_now():
-        """Returns a formatted string containing the time of day"""
+        """Returns a formatted string containing the time of day."""
         now = datetime.now()
         return "{:%H:%M}:{:05.2f}".format(now, now.second + now.microsecond / 1e6)
         # return "{:%H:%M:%S}".format(now)
 
-    def formatTime(self, record, datefmt=None):
+    def formatTime(self, record, datefmt=None):  # noqa: N802
+        """Return the Date in the specified format."""
         converter = self.converter(record.created)
         if datefmt:
             formatted_date = converter.strftime(datefmt)
@@ -87,11 +108,12 @@ class LogFormatter(logging.Formatter):
 
 
 def setup_ref(usrp, ref, num_mboards):
-    """Setup the reference clock"""
+    """Setup the reference clock."""
     if ref == "mimo":
         if num_mboards != 2:
-            logger.error("ref = \"mimo\" implies 2 motherboards; "
-                         "your system has %d boards", num_mboards)
+            logger.error(
+                'ref = "mimo" implies 2 motherboards; ' "your system has %d boards", num_mboards
+            )
             return False
         usrp.set_clock_source("mimo", 1)
     else:
@@ -115,11 +137,12 @@ def setup_ref(usrp, ref, num_mboards):
 
 
 def setup_pps(usrp, pps, num_mboards):
-    """Setup the PPS source"""
+    """Setup the PPS source."""
     if pps == "mimo":
         if num_mboards != 2:
-            logger.error("ref = \"mimo\" implies 2 motherboards; "
-                         "your system has %d boards", num_mboards)
+            logger.error(
+                'ref = "mimo" implies 2 motherboards; ' "your system has %d boards", num_mboards
+            )
             return False
         # make mboard 1 a slave over the MIMO Cable
         usrp.set_time_source("mimo", 1)
@@ -129,7 +152,7 @@ def setup_pps(usrp, pps, num_mboards):
 
 
 def check_channels(usrp, args):
-    """Check that the device has sufficient RX and TX channels available"""
+    """Check that the device has sufficient RX and TX channels available."""
     # Check RX channels
     if args.rx_rate:
         if args.rx_channels:
@@ -162,9 +185,12 @@ def check_channels(usrp, args):
 
 
 def benchmark_rx_rate(usrp, rx_streamer, random, timer_elapsed_event, rx_statistics):
-    """Benchmark the receive chain"""
-    logger.info("Testing receive rate {:.3f} Msps on {:d} channels".format(
-        usrp.get_rx_rate()/1e6, rx_streamer.get_num_channels()))
+    """Benchmark the receive chain."""
+    logger.info(
+        "Testing receive rate {:.3f} Msps on {:d} channels".format(
+            usrp.get_rx_rate() / 1e6, rx_streamer.get_num_channels()
+        )
+    )
 
     # Make a receive buffer
     num_channels = rx_streamer.get_num_channels()
@@ -175,7 +201,7 @@ def benchmark_rx_rate(usrp, rx_streamer, random, timer_elapsed_event, rx_statist
 
     # Craft and send the Stream Command
     stream_cmd = uhd.types.StreamCMD(uhd.types.StreamMode.start_cont)
-    stream_cmd.stream_now = (num_channels == 1)
+    stream_cmd.stream_now = num_channels == 1
     stream_cmd.time_spec = uhd.types.TimeSpec(usrp.get_time_now().get_real_secs() + INIT_DELAY)
     rx_streamer.issue_stream_cmd(stream_cmd)
 
@@ -197,7 +223,7 @@ def benchmark_rx_rate(usrp, rx_streamer, random, timer_elapsed_event, rx_statist
     # Receive until we get the signal to stop
     while not timer_elapsed_event.is_set():
         if random:
-            stream_cmd.num_samps = np.random.randint(1, max_samps_per_packet+1, dtype=int)
+            stream_cmd.num_samps = np.random.randint(1, max_samps_per_packet + 1, dtype=int)
             rx_streamer.issue_stream_cmd(stream_cmd)
         try:
             num_rx_samps += rx_streamer.recv(recv_buffer, metadata) * num_channels
@@ -217,8 +243,8 @@ def benchmark_rx_rate(usrp, rx_streamer, random, timer_elapsed_event, rx_statist
             # a reference to metadata.time_spec, or it would not be useful
             # further up.
             last_overflow = uhd.types.TimeSpec(
-                metadata.time_spec.get_full_secs(),
-                metadata.time_spec.get_frac_secs())
+                metadata.time_spec.get_full_secs(), metadata.time_spec.get_frac_secs()
+            )
             # If we had a sequence error, record it
             if metadata.out_of_sequence:
                 num_rx_seqerr += 1
@@ -230,8 +256,9 @@ def benchmark_rx_rate(usrp, rx_streamer, random, timer_elapsed_event, rx_statist
             num_rx_late += 1
             # Radio core will be in the idle state. Issue stream command to restart streaming.
             stream_cmd.time_spec = uhd.types.TimeSpec(
-                usrp.get_time_now().get_real_secs() + INIT_DELAY)
-            stream_cmd.stream_now = (num_channels == 1)
+                usrp.get_time_now().get_real_secs() + INIT_DELAY
+            )
+            stream_cmd.stream_now = num_channels == 1
             rx_streamer.issue_stream_cmd(stream_cmd)
         elif metadata.error_code == uhd.types.RXMetadataErrorCode.timeout:
             logger.warning("Receiver error: %s, continuing...", metadata.strerror())
@@ -252,9 +279,12 @@ def benchmark_rx_rate(usrp, rx_streamer, random, timer_elapsed_event, rx_statist
 
 
 def benchmark_tx_rate(usrp, tx_streamer, random, timer_elapsed_event, tx_statistics):
-    """Benchmark the transmit chain"""
-    logger.info("Testing transmit rate %.3f Msps on %d channels",
-                 usrp.get_tx_rate() / 1e6, tx_streamer.get_num_channels())
+    """Benchmark the transmit chain."""
+    logger.info(
+        "Testing transmit rate %.3f Msps on %d channels",
+        usrp.get_tx_rate() / 1e6,
+        tx_streamer.get_num_channels(),
+    )
 
     # Make a transmit buffer
     num_channels = tx_streamer.get_num_channels()
@@ -275,10 +305,10 @@ def benchmark_tx_rate(usrp, tx_streamer, random, timer_elapsed_event, tx_statist
             total_num_samps = np.random.randint(1, max_samps_per_packet + 1, dtype=int)
             num_acc_samps = 0
             while num_acc_samps < total_num_samps:
-                num_tx_samps += tx_streamer.send(
-                    transmit_buffer, metadata) * num_channels
-                num_acc_samps += min(total_num_samps - num_acc_samps,
-                                     tx_streamer.get_max_num_samps())
+                num_tx_samps += tx_streamer.send(transmit_buffer, metadata) * num_channels
+                num_acc_samps += min(
+                    total_num_samps - num_acc_samps, tx_streamer.get_max_num_samps()
+                )
     else:
         while not timer_elapsed_event.is_set():
             try:
@@ -302,7 +332,7 @@ def benchmark_tx_rate(usrp, tx_streamer, random, timer_elapsed_event, tx_statist
 
 
 def benchmark_tx_rate_async_helper(tx_streamer, timer_elapsed_event, tx_async_statistics):
-    """Receive and process the asynchronous TX messages"""
+    """Receive and process the asynchronous TX messages."""
     async_metadata = uhd.types.TXAsyncMetadata()
 
     # Setup the statistic counters
@@ -319,16 +349,19 @@ def benchmark_tx_rate_async_helper(tx_streamer, timer_elapsed_event, tx_async_st
             if async_metadata.event_code == uhd.types.TXMetadataEventCode.burst_ack:
                 return
             if async_metadata.event_code in (
-                    uhd.types.TXMetadataEventCode.underflow,
-                    uhd.types.TXMetadataEventCode.underflow_in_packet):
+                uhd.types.TXMetadataEventCode.underflow,
+                uhd.types.TXMetadataEventCode.underflow_in_packet,
+            ):
                 num_tx_underrun += 1
             elif async_metadata.event_code in (
-                    uhd.types.TXMetadataEventCode.seq_error,
-                    uhd.types.TXMetadataEventCode.seq_error_in_packet):
+                uhd.types.TXMetadataEventCode.seq_error,
+                uhd.types.TXMetadataEventCode.seq_error_in_packet,
+            ):
                 num_tx_seqerr += 1
             else:
-                logger.warning("Unexpected event on async recv (%s), continuing.",
-                               async_metadata.event_code)
+                logger.warning(
+                    "Unexpected event on async recv (%s), continuing.", async_metadata.event_code
+                )
     finally:
         # Write the statistics back
         tx_async_statistics["num_tx_seqerr"] = num_tx_seqerr
@@ -337,7 +370,7 @@ def benchmark_tx_rate_async_helper(tx_streamer, timer_elapsed_event, tx_async_st
 
 
 def print_statistics(rx_statistics, tx_statistics, tx_async_statistics):
-    """Print TRX statistics in a formatted block"""
+    """Print TRX statistics in a formatted block."""
     logger.debug("RX Statistics Dictionary: %s", rx_statistics)
     logger.debug("TX Statistics Dictionary: %s", tx_statistics)
     logger.debug("TX Async Statistics Dictionary: %s", tx_async_statistics)
@@ -362,12 +395,13 @@ def print_statistics(rx_statistics, tx_statistics, tx_async_statistics):
         tx_async_statistics.get("num_tx_underrun", 0),
         rx_statistics.get("num_rx_late", 0),
         tx_async_statistics.get("num_tx_timeouts", 0),
-        rx_statistics.get("num_rx_timeouts", 0))
+        rx_statistics.get("num_rx_timeouts", 0),
+    )
     logger.info(statistics_msg)
 
 
 def main():
-    """Run the benchmarking tool"""
+    """Run the benchmarking tool."""
     args = parse_args()
     # Setup some argument parsing
     if not (args.rx_rate or args.tx_rate):
@@ -378,7 +412,8 @@ def main():
     usrp = uhd.usrp.MultiUSRP(args.args)
     if usrp.get_mboard_name() == "USRP1":
         logger.warning(
-            "Benchmark results will be inaccurate on USRP1 due to insufficient features.")
+            "Benchmark results will be inaccurate on USRP1 due to insufficient features."
+        )
 
     # Always select the subdevice first, the channel mapping affects the other settings
     if args.rx_subdev:
@@ -403,9 +438,11 @@ def main():
     if not rx_channels and not tx_channels:
         # If the check returned two empty channel lists, that means something went wrong
         return False
-    logger.info("Selected %s RX channels and %s TX channels",
-                rx_channels if rx_channels else "no",
-                tx_channels if tx_channels else "no")
+    logger.info(
+        "Selected %s RX channels and %s TX channels",
+        rx_channels if rx_channels else "no",
+        tx_channels if tx_channels else "no",
+    )
 
     logger.info("Setting device timestamp to 0...")
     # If any of these conditions are met, we need to synchronize the channels
@@ -428,9 +465,10 @@ def main():
         st_args.channels = rx_channels
         st_args.args = uhd.types.DeviceAddr(args.rx_stream_args)
         rx_streamer = usrp.get_rx_stream(st_args)
-        rx_thread = threading.Thread(target=benchmark_rx_rate,
-                                     args=(usrp, rx_streamer, args.random, quit_event,
-                                           rx_statistics))
+        rx_thread = threading.Thread(
+            target=benchmark_rx_rate,
+            args=(usrp, rx_streamer, args.random, quit_event, rx_statistics),
+        )
         threads.append(rx_thread)
         rx_thread.start()
         rx_thread.setName("bmark_rx_stream")
@@ -447,15 +485,18 @@ def main():
         st_args.channels = tx_channels
         st_args.args = uhd.types.DeviceAddr(args.tx_stream_args)
         tx_streamer = usrp.get_tx_stream(st_args)
-        tx_thread = threading.Thread(target=benchmark_tx_rate,
-                                     args=(usrp, tx_streamer, args.random, quit_event,
-                                           tx_statistics))
+        tx_thread = threading.Thread(
+            target=benchmark_tx_rate,
+            args=(usrp, tx_streamer, args.random, quit_event, tx_statistics),
+        )
         threads.append(tx_thread)
         tx_thread.start()
         tx_thread.setName("bmark_tx_stream")
 
-        tx_async_thread = threading.Thread(target=benchmark_tx_rate_async_helper,
-                                           args=(tx_streamer, quit_event, tx_async_statistics))
+        tx_async_thread = threading.Thread(
+            target=benchmark_tx_rate_async_helper,
+            args=(tx_streamer, quit_event, tx_async_statistics),
+        )
         threads.append(tx_async_thread)
         tx_async_thread.start()
         tx_async_thread.setName("bmark_tx_helper")
@@ -483,7 +524,7 @@ if __name__ == "__main__":
     logger.setLevel(logging.DEBUG)
     console = logging.StreamHandler()
     logger.addHandler(console)
-    formatter = LogFormatter(fmt='[%(asctime)s] [%(levelname)s] (%(threadName)-10s) %(message)s')
+    formatter = LogFormatter(fmt="[%(asctime)s] [%(levelname)s] (%(threadName)-10s) %(message)s")
     console.setFormatter(formatter)
 
     # Vamos, vamos, vamos!
