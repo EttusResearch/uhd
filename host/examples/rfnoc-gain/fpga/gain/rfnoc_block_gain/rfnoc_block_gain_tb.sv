7597564542382f40a6c787d2054a8dc8b48c45e3
Martin Braun <martin.braun@ettus.com>
2025-04-07 10:34:52 +0200

    rfnoc: Fix testbenches for OOT modules
    This is an addition to d9f8c08, in which testbench-related code was
    moved. The RFNoC testbenches were all updated, but not the OOT-related
    testbenches (in rfnoc-gain, and in the OOT templates).

--- host/examples/rfnoc-gain/fpga/gain/rfnoc_block_gain/rfnoc_block_gain_tb.sv
+++ host/examples/rfnoc-gain/fpga/gain/rfnoc_block_gain/rfnoc_block_gain_tb.sv
@@ -18,7 +18,8 @@ module rfnoc_block_gain_tb #(
   `include "test_exec.svh"
 
   import PkgTestExec::*;
-  import PkgChdrUtils::*;
+  import rfnoc_chdr_utils_pkg::*;
+  import PkgChdrData::*;
   import PkgRfnocBlockCtrlBfm::*;
   import PkgRfnocItemUtils::*;
 
