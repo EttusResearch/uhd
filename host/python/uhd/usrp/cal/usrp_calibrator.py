ea40e65986aa090163ee15ddbfc025871e3653be
David Raeman <david@synopticengineering.com>
2025-11-25 20:21:23 +0000

    cal: Fix missing input power clamp during RX power calibration

--- host/python/uhd/usrp/cal/usrp_calibrator.py
+++ host/python/uhd/usrp/cal/usrp_calibrator.py
@@ -299,7 +299,9 @@ class USRPCalibratorBase:
                 self.log(
                     "Adapting input power to: {:+.2f} dBm.".format(usrp_input_power + power_delta)
                 )
-                usrp_input_power = self._meas_dev.set_power(usrp_input_power + power_delta)
+                usrp_input_power = self._meas_dev.set_power(
+                    min(usrp_input_power + power_delta, self.max_input_power)
+                )
                 self.log("New input power is: {:+.2f} dBm".format(usrp_input_power))
                 # And then of course, measure again
                 recvd_power = get_usrp_power(self._streamer)
