8f55f9b5858316df830289d8b90352bf89d3431b
Martin Braun <martin.braun@ettus.com>
2026-02-11 13:36:02 +0100

    lib: Fix ctrl_payload.to_string()
    - The source port was labeled 'dst_port'
    - NumData is now shown by querying the length of data_vtr
    - data_vtr[0] was always queried, which could cause problems when there
      are no data in the control payload

--- host/lib/rfnoc/chdr_types.cpp
+++ host/lib/rfnoc/chdr_types.cpp
@@ -181,15 +181,18 @@ bool ctrl_payload::operator==(const ctrl_payload& rhs) const
 
 std::string ctrl_payload::to_string() const
 {
-    return str(
-        boost::format("ctrl_payload{dst_port:%d, dst_port:%d, seq_num:%d, timestamp:%s, "
-                      "is_ack:%s, src_epid:%d, address:0x%05x, byte_enable:0x%x, "
-                      "op_code:%d, status:%d, data[0]:0x%08x}\n")
-        % dst_port % src_port % int(seq_num)
-        % (timestamp.has_value() ? str(boost::format("0x%016x") % *timestamp)
-                                 : std::string("<not present>"))
-        % (is_ack ? "true" : "false") % src_epid % address % int(byte_enable) % op_code
-        % status % data_vtr[0]);
+    return str(boost::format(
+                   "ctrl_payload{dst_port:%d, src_port:%d, seq_num:%d, timestamp:%s, "
+                   "is_ack:%s, src_epid:%d, address:0x%05x, byte_enable:0x%x, "
+                   "op_code:%d, status:%d, num_data:%d")
+               % dst_port % src_port % int(seq_num)
+               % (timestamp.has_value() ? str(boost::format("0x%016x") % *timestamp)
+                                        : std::string("<not present>"))
+               % (is_ack ? "true" : "false") % src_epid % address % int(byte_enable)
+               % op_code % status % int(data_vtr.size()))
+           + (data_vtr.empty() ? std::string()
+                               : str(boost::format(" data[0]:%08x") % data_vtr[0]))
+           + "\n";
 }
 
 //----------------------------------------------------
