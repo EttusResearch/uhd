bc017b318a9d5b8db4a5fed9e975f72886164702
Martin Anderseck <martin.anderseck@ni.com>
2026-03-02 15:53:57 +0100

    host: util: Update ADC threshold util for sub-chan
    This updates the ADC threshold utility to allow for sub-channels.

--- host/utils/x4xx_query_adc_threshold.py
+++ host/utils/x4xx_query_adc_threshold.py
@@ -5,7 +5,8 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-"""
+""" X4xx ADC Threshold Query.
+
 This script queries the minimal threshold value that still detects
 the muxed DAC tone on a X4xx device. The reported values may give an
 indication about unexpectedly high path losses in the ADC/DAC chains.
@@ -14,104 +15,103 @@ it is more important to watch major differences between the individual
 channels.
 As X410 has gain stages which are set and reset during the operation
 of this script, the results will not be completely reproducible while
-for X440 this fits quite well.
+for X440 this fits quite well. For I and Q tiles, the threshold value
+randomly reports back very high values, so only the Real tile values
+can be trusted completely, while I and Q tile threshold values may
+need to be re-run multiple times to get a reliable measurement.
 """
 
 import argparse
-import time
 import sys
+import time
+
 import uhd
 
+
 def parse_args():
-    """
-    Parse the command line arguments
-    """
+    """Parse the command line arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--args", default="", type=str)
     return parser.parse_args()
 
+
 def is_x410(usrp):
-    """
-    Returns True if executed on a X410
-    """
+    """Returns True if executed on a X410."""
     return usrp.get_mboard_name() == "x410"
 
+
 def is_x440(usrp):
-    """
-    Returns True if executed on a X440
-    """
+    """Returns True if executed on a X440."""
     return usrp.get_mboard_name() == "x440"
 
-def settings(usrp):
-    """
-    Returns the settings matching the device (X410 or X440)
-    """
-    if is_x410(usrp):
-        return {
-            "gain": 30,
-            "rx_freq": 2.5e9,
-            "tx_freq": 2.5e9
-        }
-    if is_x440(usrp):
-        return {
-            "gain": None,
-            "rx_freq": 397.55e6,
-            "tx_freq": 397.55e6
-        }
+
+def get_mixmodes(usrp):
+    """Returns the name of the mixer mode."""
+    if is_x410(usrp) or is_x440(usrp):
+        return ["Real"]
     print("Unknown x4xx device")
     sys.exit(1)
 
+
+def settings(usrp):
+    """Returns the settings matching the X4xx device."""
+    if is_x410(usrp):
+        return [{"gain": 30, "rx_freq": 2.5e9, "tx_freq": 2.5e9}]
+    if is_x440(usrp):
+        return [{"gain": None, "rx_freq": 397.55e6, "tx_freq": 397.55e6}]
+    print("Unknown x4xx device")
+    sys.exit(1)
+
+
 def set_gain(usrp, gain, channel):
-    """
-    Sets the gain for X410
-    """
-    usrp.set_rx_gain_profile("default", channel)
-    usrp.set_tx_gain_profile("default", channel)
-    usrp.set_rx_gain(gain, channel)
-    usrp.set_tx_gain(gain, channel)
-    usrp.set_rx_gain_profile("table_noatr", channel)
-    usrp.set_tx_gain_profile("table_noatr", channel)
-    usrp.set_rx_gain(0b11, "TABLE", channel)
-    usrp.set_rx_gain(0b11, "TABLE", channel)
+    """Sets the gain for X410 (X440 has no gain stages)."""
+    if is_x410(usrp):
+        usrp.set_rx_gain_profile("default", channel)
+        usrp.set_tx_gain_profile("default", channel)
+        usrp.set_rx_gain(gain, channel)
+        usrp.set_tx_gain(gain, channel)
+        usrp.set_rx_gain_profile("table_noatr", channel)
+        usrp.set_tx_gain_profile("table_noatr", channel)
+        usrp.set_rx_gain(0b11, "TABLE", channel)
+        usrp.set_rx_gain(0b11, "TABLE", channel)
+
 
 def reset_gain(usrp, channel):
-    """
-    Resets the gain values for X410 after running this script
-    """
-    usrp.set_rx_gain_profile("default", channel)
-    usrp.set_tx_gain_profile("default", channel)
-    usrp.set_rx_gain(0, channel)
-    usrp.set_tx_gain(0, channel)
+    """Resets the gain values for X410 after running this script."""
+    if is_x410(usrp):
+        usrp.set_rx_gain_profile("default", channel)
+        usrp.set_tx_gain_profile("default", channel)
+        usrp.set_rx_gain(0, channel)
+        usrp.set_tx_gain(0, channel)
 
-def check(mc, board, channel, value):
-    """
-    Checks if the given threshold value returns a valid threshold status
-    """
-    mc.setup_threshold(board, channel, 0, "hysteresis", 100, value-100, value)
+
+def check(mc, board, channel, mode, value):
+    """Checks if the given threshold value returns a valid threshold status."""
+    mc.setup_threshold(board, channel, mode, 0, "hysteresis", 10000, value - 100, value)
     time.sleep(0.01)
-    result = mc.get_threshold_status(board, channel, 0)
+    result = mc.get_threshold_status(board, channel, mode, 0)
     return (value, result)
 
-def bisect(mc, board, channel):
-    """
-    This method finds the threshold from which on the threshold status will return True
-    """
-    bad = check(mc, board, channel, 16383)
+
+def bisect(mc, board, channel, mode):
+    """This method finds the threshold from which on the threshold status will return True."""
+    bad = check(mc, board, channel, mode, 16383)
     if bad[1]:
         return bad[0]
-    good = check(mc, board, channel, 100)
+    good = check(mc, board, channel, mode, 100)
     if not good[1]:
         return good[0]
     while (good[1] != bad[1]) and (abs(good[0] - bad[0]) > 1):
         upper = good[0]
         lower = bad[0]
         value = lower + (upper - lower) // 2
-        result = check(mc, slot, channel, value)
-        if result[1] :
+        result = check(mc, board, channel, mode, value)
+        if result[1]:
             good = result
         else:
             bad = result
     return good[0] if good[1] else None
+
 
 if __name__ == "__main__":
     print("Determine minimal threshold value to be detected on ADC")
@@ -121,20 +121,23 @@ if __name__ == "__main__":
     mc = usrp.get_mpm_client()
     mc.set_dac_mux_data(0x7FFF, 0)
     ch_per_board = usrp.get_rx_num_channels() // 2
+
     print("Channel | Threshold")
     print("--------+----------")
     for i in range(usrp.get_rx_num_channels()):
-        print(f"{i:7} | ", end="")
-        if is_x410(usrp):
-            set_gain(usrp, settings["gain"], i)
-        usrp.set_rx_antenna("CAL_LOOPBACK", i)
-        usrp.set_tx_antenna("CAL_LOOPBACK", i)
-        usrp.set_rx_freq(settings["rx_freq"], i)
-        usrp.set_tx_freq(settings["tx_freq"], i)
-        slot = i // ch_per_board
-        chan = i % ch_per_board
-        mc.set_dac_mux_enable(slot, chan, 1)
-        print(f"{bisect(mc, slot, chan):9}")
-        mc.set_dac_mux_enable(slot, chan, 0)
-        if is_x410(usrp):
+        for j, mode in enumerate(get_mixmodes(usrp)):
+            CH_NAME = f"{i} {mode}"
+            print(f"{CH_NAME:7} | ", end="")
+            set_gain(usrp, settings[j]["gain"], i)
+            usrp.set_rx_antenna("CAL_LOOPBACK", i)
+            usrp.set_tx_antenna("CAL_LOOPBACK", i)
+            usrp.set_rx_freq(settings[j]["rx_freq"], i)
+            usrp.set_tx_freq(settings[j]["tx_freq"], i)
+            slot = i // ch_per_board
+            chan = i % ch_per_board
+            # I DAC Mux needs to be enabled for both I and Q subchannels,
+            # so just turn them all on (5: All)
+            mc.set_dac_mux_enable(slot, chan, 1, 5)
+            print(f"{bisect(mc, slot, chan, j):9}")
+            mc.set_dac_mux_enable(slot, chan, 0, 5)
             reset_gain(usrp, i)
